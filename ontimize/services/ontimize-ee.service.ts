59d41db49f5c852e13dfa8f1df56c5cb8462ec6f
Gonzalo Martínez <gonzalo.martinez@imatia.com>
2017-03-06 08:26:03 +0100

    Fixing bugs

--- ontimize/services/ontimize-ee.service.ts
+++ ontimize/services/ontimize-ee.service.ts
@@ -1,13 +1,13 @@
-import {Injector, Injectable} from '@angular/core';
-import {Http, Headers, RequestOptions, RequestMethod} from '@angular/http';
-import {Router} from '@angular/router';
-import {Observable} from 'rxjs/Observable';
+import { Injector, Injectable } from '@angular/core';
+import { Http, Headers, RequestOptions, RequestMethod } from '@angular/http';
+import { Router } from '@angular/router';
+import { Observable } from 'rxjs/Observable';
 import 'rxjs/add/operator/map';
 import 'rxjs/add/operator/share';
 
-import {IAuthService, IDataService} from '../interfaces';
-import {LoginService, SERVICE_CONFIG} from '../services';
-import {APP_CONFIG, Config} from '../config/app-config';
+import { IAuthService, IDataService } from '../interfaces';
+import { LoginService, SERVICE_CONFIG } from '../services';
+import { APP_CONFIG, Config } from '../config/app-config';
 
 
 @Injectable()
@@ -25,10 +25,10 @@ export class OntimizeEEService implements IAuthService, IDataService {
   protected http: Http;
   protected _sessionid: string;
   protected _urlBase: string;
-  protected _appConfig:Config;
+  protected _appConfig: Config;
 
 
-  constructor( protected injector: Injector) {
+  constructor(protected injector: Injector) {
 
     this.http = this.injector.get(Http);
     this._appConfig = this.injector.get(APP_CONFIG);
@@ -47,7 +47,7 @@ export class OntimizeEEService implements IAuthService, IDataService {
   }
 
   public configureService(config: any): void {
-    this._urlBase = config.urlBase ? config.urlBase : this._appConfig['apiEndpoint'] ;
+    this._urlBase = config.urlBase ? config.urlBase : this._appConfig['apiEndpoint'];
     this._sessionid = config.session ? config.session.id : -1;
     this.path = config.path;
     //TODO init other params
@@ -117,13 +117,13 @@ export class OntimizeEEService implements IAuthService, IDataService {
     av = (this.isNullOrUndef(av)) ? this.av : av;
     sqltypes = (this.isNullOrUndef(sqltypes)) ? this.sqltypes : sqltypes;
 
-    var url = this._urlBase + this.path +'/'+ entity + '/search';
+    var url = this._urlBase + this.path + '/' + entity + '/search';
 
     var headers: Headers = new Headers();
     headers.append('Access-Control-Allow-Origin', '*');
     headers.append('Content-Type', 'application/json;charset=UTF-8');
 
-    let authorizationToken = 'Bearer '+ this._sessionid;
+    let authorizationToken = 'Bearer ' + this._sessionid;
     headers.append('Authorization', authorizationToken);
 
     var body = JSON.stringify({
@@ -134,7 +134,7 @@ export class OntimizeEEService implements IAuthService, IDataService {
 
     let _innerObserver: any;
     let dataObservable = new Observable(observer =>
-        _innerObserver = observer).share();
+      _innerObserver = observer).share();
 
     var self = this;
     this.http
@@ -222,30 +222,30 @@ export class OntimizeEEService implements IAuthService, IDataService {
       */
     let _innerObserver: any;
     let dataObservable = new Observable(observer =>
-        _innerObserver = observer).share();
+      _innerObserver = observer).share();
 
     return dataObservable;
   }
 
   public insert(av: Object = {}, entity: string, sqltypes?: Object): Observable<any> {
 
-    var url = this._urlBase + this.path +'/'+ entity;
+    var url = this._urlBase + this.path + '/' + entity;
 
     var headers: Headers = new Headers();
     headers.append('Access-Control-Allow-Origin', '*');
     headers.append('Content-Type', 'application/json;charset=UTF-8');
 
-    let authorizationToken = 'Bearer '+ this._sessionid;
+    let authorizationToken = 'Bearer ' + this._sessionid;
     headers.append('Authorization', authorizationToken);
 
     var body = JSON.stringify({
       data: av,
-      sqltypes : sqltypes
+      sqltypes: sqltypes
     });
 
     let _innerObserver: any;
     let dataObservable = new Observable(observer =>
-        _innerObserver = observer).share();
+      _innerObserver = observer).share();
 
     var self = this;
     this.http
@@ -262,7 +262,7 @@ export class OntimizeEEService implements IAuthService, IDataService {
           //Unknow state -> error
           _innerObserver.error('Service unavailable');
         }
-      },  error => {
+      }, error => {
         if (error.status === 401) {
           self.redirectLogin(true);
         } else {
@@ -276,23 +276,24 @@ export class OntimizeEEService implements IAuthService, IDataService {
 
   public update(kv: Object = {}, av: Object = {}, entity?: string, sqltypes?: Object): Observable<any> {
 
-    var url = this._urlBase + this.path +'/'+ entity;
+    var url = this._urlBase + this.path + '/' + entity;
 
     var headers: Headers = new Headers();
     headers.append('Access-Control-Allow-Origin', '*');
     headers.append('Content-Type', 'application/json;charset=UTF-8');
 
-    let authorizationToken = 'Bearer '+ this._sessionid;
+    let authorizationToken = 'Bearer ' + this._sessionid;
     headers.append('Authorization', authorizationToken);
 
     var body = JSON.stringify({
       filter: kv,
-      data: av
+      data: av,
+      sqltypes: sqltypes
     });
 
     let _innerObserver: any;
     let dataObservable = new Observable(observer =>
-        _innerObserver = observer).share();
+      _innerObserver = observer).share();
 
     var self = this;
     this.http
@@ -309,7 +310,7 @@ export class OntimizeEEService implements IAuthService, IDataService {
           //Unknow state -> error
           _innerObserver.error('Service unavailable');
         }
-      },  error => {
+      }, error => {
         if (error.status === 401) {
           self.redirectLogin(true);
         } else {
@@ -323,28 +324,29 @@ export class OntimizeEEService implements IAuthService, IDataService {
 
   public delete(kv: Object = {}, entity?: string, sqltypes?: Object): Observable<any> {
 
-    var url = this._urlBase + this.path +'/'+ entity;
+    var url = this._urlBase + this.path + '/' + entity;
 
     var headers: Headers = new Headers();
     headers.append('Access-Control-Allow-Origin', '*');
     headers.append('Content-Type', 'application/json;charset=UTF-8');
 
-    let authorizationToken = 'Bearer '+ this._sessionid;
+    let authorizationToken = 'Bearer ' + this._sessionid;
     headers.append('Authorization', authorizationToken);
 
     var body = JSON.stringify({
-      filter: kv
+      filter: kv,
+      sqltypes: sqltypes
     });
 
     let options = new RequestOptions({
-      method : RequestMethod.Delete,
-      headers : headers,
-      body : body
+      method: RequestMethod.Delete,
+      headers: headers,
+      body: body
     });
 
     let _innerObserver: any;
     let dataObservable = new Observable(observer =>
-        _innerObserver = observer).share();
+      _innerObserver = observer).share();
 
     var self = this;
     this.http
@@ -361,7 +363,7 @@ export class OntimizeEEService implements IAuthService, IDataService {
           //Unknow state -> error
           _innerObserver.error('Service unavailable');
         }
-      },  error => {
+      }, error => {
         if (error.status === 401) {
           self.redirectLogin(true);
         } else {
