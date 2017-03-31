5220582d6cc3ce730dcdb371ba51e4f5d0f43c37
Gonzalo Martínez <gonzalo.martinez@imatia.com>
2017-03-31 08:38:03 +0200

    Fixing table header bug

--- ontimize/components/table/o-table.component.ts
+++ ontimize/components/table/o-table.component.ts
@@ -827,7 +827,9 @@ export class OTableComponent extends OServiceComponent implements OnInit, OnDest
     }
     this.table = this.tableHtmlEl.DataTable(this.dataTableOptions);
     new ($ as any).fn.dataTable.FixedHeader(this.table, {
-      header: true
+      header: true,
+      forceFloating: true,
+      scrollWidth: 20
     });
     this.parseTableOptions();
 
