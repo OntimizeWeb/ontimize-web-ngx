bba866791e0b0bacf839e251b3d56696450ccccf
Gonzalo Martínez <gonzalo.martinez@imatia.com>
2017-04-27 16:06:25 +0200

    Including ODialogComponent in OSharedModule

--- ontimize/shared.module.ts
+++ ontimize/shared.module.ts
@@ -7,6 +7,7 @@ import { MdTooltipModule } from '@angular/material';
 
 import { DisabledComponentDirective } from './directives/DisabledComponentDirective';
 import { OListItemDirective } from './components/list/list-item/o-list-item.directive';
+import { ODialogComponent } from './components/dialog/o-dialog.component';
 
 import { OrderByPipe } from './pipes';
 import { ONTIMIZE_DIRECTIVES } from './config/o-directives';
@@ -20,6 +21,9 @@ import { ONTIMIZE_DIRECTIVES } from './config/o-directives';
   ],
   imports: [
     MdTooltipModule
+  ],
+  entryComponents: [
+    ODialogComponent
   ],
   exports: [
     DisabledComponentDirective,
