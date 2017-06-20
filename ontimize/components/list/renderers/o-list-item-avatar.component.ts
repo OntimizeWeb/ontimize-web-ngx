b267c51c76992a46b0e9be309b452ed04de28ccb
Gonzalo Martínez <gonzalo.martinez@imatia.com>
2017-06-20 13:11:02 +0200

    Updating list

--- ontimize/components/list/renderers/o-list-item-avatar.component.ts
+++ ontimize/components/list/renderers/o-list-item-avatar.component.ts
@@ -7,9 +7,12 @@ import {
   ElementRef,
   Renderer,
   Optional,
-  NgModule
+  NgModule,
+  OnInit,
+  AfterViewInit
 } from '@angular/core';
 
+import { DomSanitizer, SafeResourceUrl } from '@angular/platform-browser';
 import { CommonModule } from '@angular/common';
 
 import { OListItemComponent } from '../list-item/o-list-item.component';
@@ -18,7 +21,12 @@ import { OSharedModule } from '../../../shared';
 
 export const DEFAULT_INPUTS_O_LIST_ITEM_AVATAR = [
   ...OListItemTextRenderer.DEFAULT_INPUTS_O_TEXT_RENDERER,
-  'avatar'
+  'avatar',
+
+  'emptyAvatar: empty-avatar',
+
+  // avatar-type [base64|url]: avatar type (extern url or base64). Default: no value.
+  'avatarType: avatar-type'
 ];
 
 export const DEFAULT_OUTPUTS_O_LIST_ITEM_AVATAR = [
@@ -38,15 +46,18 @@ export const DEFAULT_OUTPUTS_O_LIST_ITEM_AVATAR = [
   encapsulation: ViewEncapsulation.None
 })
 
-export class OListItemAvatarComponent extends OListItemTextRenderer {
+export class OListItemAvatarComponent extends OListItemTextRenderer implements OnInit, AfterViewInit {
 
   protected avatar: string;
-
+  protected avatarType: string;
+  protected emptyAvatar: string;
+  protected avatarSrc: SafeResourceUrl;
   constructor(
     elRef: ElementRef,
     _renderer: Renderer,
     _injector: Injector,
-    @Optional() @Inject(forwardRef(() => OListItemComponent)) protected _listItem: OListItemComponent
+    @Optional() @Inject(forwardRef(() => OListItemComponent)) protected _listItem: OListItemComponent,
+    public sanitizer: DomSanitizer
   ) {
     super(elRef, _renderer, _injector, _listItem);
     this.elRef.nativeElement.classList.add('o-list-item-avatar');
@@ -54,6 +65,25 @@ export class OListItemAvatarComponent extends OListItemTextRenderer {
 
   ngAfterViewInit() {
     this.modifyMdListItemElement();
+  }
+
+  ngOnInit() {
+    let avatarValue: any = this.avatar;
+    if (!this.avatar) {
+      avatarValue = this.emptyAvatar;
+    } else {
+      switch (this.avatarType) {
+        case 'base64':
+          avatarValue = ('data:image/*;base64,' + ((typeof (avatarValue.bytes) !== 'undefined') ? avatarValue.bytes : avatarValue));
+          break;
+        case 'url':
+        default:
+          avatarValue = this.avatar;
+          break;
+      }
+    }
+    this.avatarSrc = this.sanitizer.bypassSecurityTrustResourceUrl(avatarValue);
+
   }
 }
 
