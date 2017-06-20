7b684359ae7f6daccd7a5481b646e1a90b9f753f
Gonzalo Martínez <gonzalo.martinez@imatia.com>
2017-06-20 12:19:47 +0200

    Updating app-header

--- ontimize/components/app-header/o-app-header.component.ts
+++ ontimize/components/app-header/o-app-header.component.ts
@@ -1,16 +1,23 @@
 import {
   Injector,
+  OnDestroy,
   NgModule,
   Component,
-  EventEmitter
+  EventEmitter,
+  ElementRef,
+  ViewEncapsulation
 } from '@angular/core';
-
 import { Router } from '@angular/router';
 import { CommonModule } from '@angular/common';
+import { Subscription } from 'rxjs/Subscription';
 
 import { OSharedModule } from '../../shared';
 import { AppConfig } from '../../config/app-config';
-import { DialogService, OTranslateService } from '../../services';
+import {
+  DialogService,
+  OTranslateService,
+  OModulesInfoService
+} from '../../services';
 
 import { OUserInfoModule } from '../../components';
 
@@ -27,28 +34,44 @@ export const DEFAULT_OUTPUTS_O_APP_HEADER = [
   inputs: DEFAULT_INPUTS_O_APP_HEADER,
   outputs: DEFAULT_OUTPUTS_O_APP_HEADER,
   template: require('./o-app-header.component.html'),
-  styles: [require('./o-app-header.component.scss')]
+  styles: [require('./o-app-header.component.scss')],
+  encapsulation: ViewEncapsulation.None
 })
-export class OAppHeaderComponent {
+export class OAppHeaderComponent implements OnDestroy {
 
   public static DEFAULT_INPUTS_O_APP_HEADER = DEFAULT_INPUTS_O_APP_HEADER;
   public static DEFAULT_OUTPUTS_O_APP_HEADER = DEFAULT_OUTPUTS_O_APP_HEADER;
 
   protected dialogService: DialogService;
   protected translateService: OTranslateService;
+  protected modulesInfoService: OModulesInfoService;
   protected _config: AppConfig;
   protected availableLangs: string[];
+  protected headerTitle: '';
 
+  modulesInfoSubscription: Subscription;
   toggleSidenav = new EventEmitter<void>();
 
   constructor(
     private router: Router,
-    protected injector: Injector
+    protected injector: Injector,
+    protected elRef: ElementRef
   ) {
     this.dialogService = this.injector.get(DialogService);
     this.translateService = this.injector.get(OTranslateService);
     this._config = this.injector.get(AppConfig);
     this.availableLangs = this._config.getConfiguration().applicationLocales;
+    this.modulesInfoService = this.injector.get(OModulesInfoService);
+
+    // this.userInfo = this.modulesInfoService.();
+    this.modulesInfoSubscription = this.modulesInfoService.getModuleChangeObservable().subscribe(res => {
+      this.headerTitle = res.name;
+    });
+    this.elRef.nativeElement.classList.add('o-app-header');
+  }
+
+  ngOnDestroy() {
+    this.modulesInfoSubscription.unsubscribe();
   }
 
   onLogoutClick() {
