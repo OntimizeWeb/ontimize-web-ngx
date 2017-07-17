5641744fbf7ab32c9e20ffd9f64205f9af1a6ac5
Gonzalo Martínez <gonzalo.martinez@imatia.com>
2017-07-17 09:09:25 +0200

    Fixing OComponent decorator bug

--- ontimize/decorators/o-component.ts
+++ ontimize/decorators/o-component.ts
@@ -1,32 +1,50 @@
 import { Component } from '@angular/core';
+import 'reflect-metadata';
 
-const _reflect: any = Reflect;
+export function OComponent(newCompAnnotations: any) {
 
-export function OComponent(annotation: any) {
   function OComponentInner(target: Function) {
     var parentTarget = Object.getPrototypeOf(target.prototype).constructor;
-    var parentAnnotations = _reflect.getMetadata('annotations', parentTarget);
-    var parentAnnotation = parentAnnotations[0];
-    Object.keys(parentAnnotation).forEach(key => {
-      if (isPresent(parentAnnotation[key])) {
-        if (!isPresent(annotation[key])) {
-          if (typeof annotation[key] === 'function') {
-            annotation[key] = annotation[key].call(this, parentAnnotation[key]);
-          } else {
-            annotation[key] = parentAnnotation[key];
-          }
-        }
-      }
-      if (!isPresent(annotation[key])) {
-        annotation[key] = parentAnnotation[key];
-      }
-    });
-    var metadata = new Component(annotation);
-    _reflect.defineMetadata('annotations', [metadata], target);
+    var parentAnnotations = Reflect.getMetadata('annotations', parentTarget);
+    if (parentAnnotations) {
+      var parentAnnotation = parentAnnotations[0];
+      copyDecorators(newCompAnnotations, parentAnnotation);
+    } else if (parentTarget.hasOwnProperty('decorators')) {
+      var parentDecorators = getDecorators(parentTarget);
+      copyDecorators(newCompAnnotations, parentDecorators);
+    }
+    var metadata = new Component(newCompAnnotations);
+    Reflect.defineMetadata('annotations', [metadata], target);
   }
   return OComponentInner;
 }
 
 function isPresent(obj) {
   return obj !== undefined && obj !== null;
+}
+
+function getDecorators(target: any) {
+  let decoratorsObj = undefined;
+  const decorators = target.decorators[0];
+  if (decorators && decorators.args && decorators.args[0]) {
+    decoratorsObj = decorators.args[0];
+  }
+  return decoratorsObj;
+}
+
+function copyDecorators(newComp: any, parent: any) {
+  Object.keys(parent).forEach(key => {
+    if (isPresent(parent[key])) {
+      if (!isPresent(newComp[key])) {
+        if (typeof newComp[key] === 'function') {
+          newComp[key] = newComp[key].call(this, parent[key]);
+        } else {
+          newComp[key] = parent[key];
+        }
+      }
+    }
+    if (!isPresent(newComp[key])) {
+      newComp[key] = parent[key];
+    }
+  });
 }
