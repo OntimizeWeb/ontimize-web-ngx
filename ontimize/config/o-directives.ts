b4417ff142da3518b78e0e233e5ff4bf743e50ce
gonzalo.martinez <gonzalo.martinez@imatia.com>
2017-09-08 12:06:42 +0200

    Deleting unnecesary directives

--- ontimize/config/o-directives.ts
+++ ontimize/config/o-directives.ts
@@ -1,10 +1,6 @@
-import { DisabledComponentDirective } from '../directives/DisabledComponentDirective';
-import { FormComponentDirective } from '../directives/FormComponentDirective';
 import { OListItemDirective } from '../components/list/list-item/o-list-item.directive';
 
 export const ONTIMIZE_DIRECTIVES = [
   //Ontimize directives...
-  DisabledComponentDirective,
-  FormComponentDirective,
   OListItemDirective
 ];
