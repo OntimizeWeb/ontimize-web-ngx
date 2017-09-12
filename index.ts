e730138d6001dd89e502d5fdaf9070be72ff9c0f
gonzalo.martinez <gonzalo.martinez@imatia.com>
2017-09-12 16:30:02 +0200

    Changing file location

---
+++ index.ts
@@ -0,0 +1,32 @@
+import { NgModule } from '@angular/core';
+
+import {
+  INTERNAL_ONTIMIZE_MODULES_EXPORTED,
+  INTERNAL_ONTIMIZE_MODULES
+} from './ontimize/config/o-modules';
+
+export { ONTIMIZE_MODULES } from './ontimize/config/o-modules';
+export { ONTIMIZE_PROVIDERS } from './ontimize/config/o-providers';
+
+export * from './ontimize/config/app-config';
+export * from './ontimize/MainLauncher';
+export * from './ontimize/pipes';
+export * from './ontimize/services';
+export * from './ontimize/decorators';
+export * from './ontimize/components';
+export * from './ontimize/layouts';
+export * from './ontimize/utils';
+export * from './ontimize/shared';
+export * from './ontimize/validators/o-validators';
+
+import { ODialogComponent } from './ontimize/components';
+
+@NgModule({
+  imports: INTERNAL_ONTIMIZE_MODULES,
+  exports: INTERNAL_ONTIMIZE_MODULES_EXPORTED,
+  entryComponents: [
+    ODialogComponent
+  ]
+})
+export class OntimizeWebModule {
+}
