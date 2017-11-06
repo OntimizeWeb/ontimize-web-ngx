d026ae730e455974b9a6e95bbba55d4bd0d2127b
gonzalo.martinez <gonzalo.martinez@imatia.com>
2017-11-06 13:09:17 +0100

    New undo feature and refactoring all cache isues to o-form-cache.class

---
+++ ontimize/components/form/cache/o-form.cache.class.ts
@@ -0,0 +1,154 @@
+
+import { Subscription } from 'rxjs/Subscription';
+import { IFormControlComponent } from '../../o-form-data-component.class';
+import { OFormComponent } from '../o-form.component';
+
+export class OFormCacheClass {
+
+  protected initialDataCache: Object = {};
+  protected formDataCache: Object;
+  protected valueChangesStack: Array<any> = [];
+  protected _componentsSubscritpions: Array<Subscription> = [];
+  protected blockCaching: boolean = false;
+  protected initializedCache: boolean = false;
+
+  protected formCacheSubscription: Subscription;
+
+  constructor(protected form: OFormComponent) {
+  }
+
+  registerFormGroupListener() {
+    /*
+    * Keeping updated a cache of form data values
+    */
+    var self = this;
+    this.formCacheSubscription = this.form.formGroup.valueChanges.subscribe((value: any) => {
+      if (self.formDataCache === undefined) {
+        // initialize cache
+        self.formDataCache = {};
+      }
+      Object.assign(self.formDataCache, value);
+    });
+  }
+
+  protected addChangeToStack(attr: string, comp: IFormControlComponent) {
+    const currentValue = comp.getFormControl().value;
+    this.valueChangesStack.push({
+      attr: attr,
+      value: currentValue
+    });
+  }
+
+  registerComponentCaching(attr: string, comp: IFormControlComponent) {
+    const self = this;
+    this._componentsSubscritpions[attr] = comp.getFormControl().valueChanges.subscribe(value => {
+      // this._componentsSubscritpions[attr] = comp.onChange.subscribe(value => {
+      if (self.initializedCache && !self.blockCaching && self.hasComponentChanged(attr, comp)) {
+        self.addChangeToStack(attr, comp);
+      }
+    });
+  }
+
+  unregisterComponentCaching(attr: string) {
+    if (this._componentsSubscritpions[attr]) {
+      this._componentsSubscritpions[attr].unsubscribe();
+      delete this._componentsSubscritpions[attr];
+    }
+  }
+
+  getCachedValue(attr: string): any {
+    if (this.formDataCache && this.formDataCache.hasOwnProperty(attr)) {
+      return this.formDataCache[attr];
+    }
+    return undefined;
+  }
+
+  destroy() {
+    if (this.formCacheSubscription) {
+      this.formCacheSubscription.unsubscribe();
+    }
+    this.formDataCache = undefined;
+  }
+
+  initializeCache(val: any) {
+    this.initialDataCache = val;
+    this.valueChangesStack = [];
+    this.initializedCache = true;
+  }
+
+  getInitialDataCache() {
+    return this.initialDataCache;
+  }
+
+  getDataCache() {
+    return this.formDataCache;
+  }
+
+  restartCache() {
+    this.formDataCache = {};
+    this.initializeCache({});
+  }
+
+  setCacheSnapshot() {
+    this.initializeCache(this.getDataCache());
+  }
+
+  undoLastChange(options?) {
+    options = (options || {});
+    //removing last element because it is the last changed value
+    const lastElement = this.valueChangesStack.pop();
+    // && !options.keyboardEvent
+    if (lastElement) {
+      const lastCacheValue = this.getCacheLastValue(lastElement.attr);
+      const lastValue = (lastCacheValue !== null) ? lastCacheValue : this.initialDataCache[lastElement.attr];
+      this.setFormControlValue(lastElement.attr, lastValue);
+    }
+  }
+
+  protected setFormControlValue(attr: string, val: any) {
+    this.blockCaching = true;
+    this.form.formGroup.get(attr).setValue(val);
+    this.blockCaching = false;
+  }
+
+  protected hasComponentChanged(attr: string, comp: IFormControlComponent): boolean {
+    const currentValue = comp.getFormControl().value;
+    const cache = this.formDataCache || this.initialDataCache;
+    return (currentValue !== cache[attr]);
+  }
+
+  protected getCacheLastValue(attr: string): any {
+    let result = null;
+    for (let i = this.valueChangesStack.length - 1; i >= 0; i--) {
+      const current = this.valueChangesStack[i];
+      if (current.attr === attr) {
+        result = current.value;
+        break;
+      }
+    }
+    return result;
+  }
+
+  get isCacheStackEmpty(): boolean {
+    return (this.valueChangesStack.length === 0);
+  }
+
+  isInitialStateChanged(): boolean {
+    let res = false;
+    let initialKeys = Object.keys(this.initialDataCache);
+    let currentKeys = Object.keys(this.formDataCache);
+    if (initialKeys.length !== currentKeys.length) {
+      return true;
+    }
+    for (let i = 0, leni = initialKeys.length; i < leni; i++) {
+      let key = initialKeys[i];
+      // TODO be careful with types comparisions
+      res = (this.initialDataCache[key] !== this.formDataCache[key]);
+      if (res) {
+        break;
+      }
+    }
+    return res;
+  }
+
+}
