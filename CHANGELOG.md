0d69818c38db21382efc23a726e155db354420c3
gonzalo.martinez <gonzalo.martinez@imatia.com>
2017-11-20 15:12:41 +0100

    Updating column filtering

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -4,7 +4,7 @@
 * **o-table-button**: adding '*icon-position*' input ([66f0acb](https://github.com/OntimizeWeb/ontimize-web-ng2/commit/66f0acb))
 * **o-table**: adding public methods *'getValue'*, *'getRendererValue'*, *'getSqlTypes'* ([236b9b6](https://github.com/OntimizeWeb/ontimize-web-ng2/commit/236b9b6))
 * **o-table-option**: new '*o-table-option*' component for adding options to '*o-table*' menu ([f48a591](https://github.com/OntimizeWeb/ontimize-web-ng2/commit/f48a591))
-* **o-table, o-table-columns-filter**: new '*o-table-columns-filter*' component ([a0a3eed](https://github.com/OntimizeWeb/ontimize-web-ng2/commit/a0a3eed))
+* **o-table, o-table-columns-filter**: new '*o-table-columns-filter*' component ([a0a3eed](https://github.com/OntimizeWeb/ontimize-web-ng2/commit/a0a3eed)) ([21ad0b7](https://github.com/OntimizeWeb/ontimize-web-ng2/commit/21ad0b7))
 * **o-table-paginator**: new '*o-table-paginator*' component ([23b4e4d](https://github.com/OntimizeWeb/ontimize-web-ng2/commit/23b4e4d)) ([2213a0d](https://github.com/OntimizeWeb/ontimize-web-ng2/commit/2213a0d))
 
 
--- ontimize/components/table/extensions/header/table-columns-filter/o-table-columns-filter.component.ts
+++ ontimize/components/table/extensions/header/table-columns-filter/o-table-columns-filter.component.ts
@@ -1,5 +1,5 @@
 import { Component, Inject, forwardRef, OnInit, Injector } from '@angular/core';
-import { OTableComponent } from '../../../o-table.component';
+import { OTableComponent, OColumn } from '../../../o-table.component';
 import { Util } from '../../../../../utils';
 
 export const DEFAULT_INPUTS_O_TABLE_COLUMN_FILTER = [
@@ -22,9 +22,14 @@ export class OTableColumnsFilterComponent implements OnInit {
 
   public static DEFAULT_INPUTS_O_TABLE_COLUMN_FILTER = DEFAULT_INPUTS_O_TABLE_COLUMN_FILTER;
   public static DEFAULT_OUTPUTS_O_TABLE_COLUMN_FILTER = DEFAULT_OUTPUTS_O_TABLE_COLUMN_FILTER;
+  public static COMPARISON_TYPE_SEPARATOR = ':';
+
+  public static DEFAULT_COMPARISON_TYPE = 'VIEW';
+  public static MODEL_COMPARISON_TYPE = 'MODEL';
 
   protected columns: string;
   protected columnsArray: Array<string> = [];
+  protected columnsComparisonProperty: Object = {};
 
   constructor(
     protected injector: Injector,
@@ -34,6 +39,32 @@ export class OTableColumnsFilterComponent implements OnInit {
 
   ngOnInit() {
     this.columnsArray = Util.parseArray(this.columns);
-    this.table.setFilterableColumns(this.columnsArray);
+    if (this.columnsArray.length === 0) {
+      this.columnsArray = this.table.oTableOptions.visibleColumns;
+    }
+    const self = this;
+    this.columnsArray.map((colData, i, arr) => {
+      let colDef = colData.split(OTableColumnsFilterComponent.COMPARISON_TYPE_SEPARATOR);
+      let colName = colDef[0];
+      let compType = (colDef[1] || '').toUpperCase();
+      if ([OTableColumnsFilterComponent.DEFAULT_COMPARISON_TYPE, OTableColumnsFilterComponent.MODEL_COMPARISON_TYPE].indexOf(compType) === -1) {
+        compType = OTableColumnsFilterComponent.DEFAULT_COMPARISON_TYPE;
+      }
+      arr[i] = colName;
+      self.columnsComparisonProperty[colName] = compType;
+    });
+    this.table.setOTableColumnsFilter(this);
+  }
+
+  isColumnFilterable(attr: string) {
+    return (this.columnsArray.indexOf(attr) !== -1);
+  }
+
+  getColumnComparisonValue(column: OColumn, val: any): any {
+    if (!column || this.columnsComparisonProperty[column.attr] === OTableColumnsFilterComponent.MODEL_COMPARISON_TYPE) {
+      return val;
+    } else {
+      return column.renderer ? column.renderer.getCellData(val) : val;
+    }
   }
 }
