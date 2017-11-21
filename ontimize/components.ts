b3a653fa6dda77c0e8657509439b2e885b99c3b2
gonzalo.martinez <gonzalo.martinez@imatia.com>
2017-11-21 13:15:55 +0100

    Updating table inner compnents exportation

--- ontimize/components.ts
+++ ontimize/components.ts
@@ -38,10 +38,7 @@ export * from './components/side-menu/o-side-menu-group.component';
 export * from './components/side-menu/o-side-menu-item.component';
 export * from './components/side-menu/o-locale-side-menu-item.component';
 export * from './components/side-menu/o-side-menu-separator.component';
-export * from './components/table/o-table.component';
-export * from './components/table/column/o-table-column.component';
-export * from './components/table/column/cell-renderer/cell-renderer';
-export * from './components/table/extensions/header/o-table-header-components';
+export * from './components/table/o-table-components';
 
 export * from './components/o-component.class';
 export * from './components/o-form-data-component.class';
---
+++ ontimize/components/table/extensions/footer/o-table-footer-components.ts
@@ -0,0 +1,1 @@
+export * from './paginator/o-table-paginator.component';
