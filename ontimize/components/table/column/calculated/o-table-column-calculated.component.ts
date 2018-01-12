d8479d6856f2d871939489145886fd4dc4f45946
Patricia Martinez <patricia.martinez@imatia.com>
2018-01-12 09:39:50 +0100

    updating o-table-column-calculated

--- ontimize/components/table/column/calculated/o-table-column-calculated.component.ts
+++ ontimize/components/table/column/calculated/o-table-column-calculated.component.ts
@@ -2,52 +2,55 @@ import { Component, Inject, forwardRef, Injector, ComponentFactoryResolver } fro
 import { OTableComponent } from '../../o-table.component';
 import { OTableColumnComponent, DEFAULT_INPUTS_O_TABLE_COLUMN } from '../o-table-column.component';
 
-
 export const DEFAULT_INPUTS_O_TABLE_COLUMN_CALCULATED = [
-    ...DEFAULT_INPUTS_O_TABLE_COLUMN,
-    // operation [string]: operation .
-    'operation',
-    // function-operation [funtion]: callback title. Default: no value.
-    'functionOperation:function-operation'
+  ...DEFAULT_INPUTS_O_TABLE_COLUMN,
+  // operation [string]: operation .
+  'operation',
+  // function-operation [funtion]: callback title. Default: no value.
+  'functionOperation:function-operation'
 ];
 
 export type OperatorFunction = (value: any[]) => number;
 
 export class OColumnCalculated {
-    operator: string | OperatorFunction;
+  operator: string | OperatorFunction;
 }
 
 @Component({
-    selector: 'o-table-column-calculated',
-    templateUrl: './o-table-column-calculated.component.html',
-    inputs: [
-        ...DEFAULT_INPUTS_O_TABLE_COLUMN_CALCULATED,
-    ],
-    providers: [OTableColumnComponent]
-
+  selector: 'o-table-column-calculated',
+  templateUrl: './o-table-column-calculated.component.html',
+  inputs: [
+    ...DEFAULT_INPUTS_O_TABLE_COLUMN_CALCULATED,
+  ],
+  providers: [
+    {
+      provide: OTableColumnComponent,
+      useExisting: forwardRef(() => OTableColumnCalculatedComponent)
+    }
+  ]
 })
 
 export class OTableColumnCalculatedComponent extends OTableColumnComponent {
 
-    public operation: string = '';
+  public operation: string = '';
 
-    protected _functionAggregate: OperatorFunction;
+  protected _functionAggregate: OperatorFunction;
 
-    get functionAggregate(): OperatorFunction {
-        return this._functionAggregate;
-    }
+  get functionAggregate(): OperatorFunction {
+    return this._functionAggregate;
+  }
 
-    set functionAggregate(val: OperatorFunction) {
-        this._functionAggregate = val;
-    }
+  set functionAggregate(val: OperatorFunction) {
+    this._functionAggregate = val;
+  }
 
 
-    constructor(
-        @Inject(forwardRef(() => OTableComponent)) public table: OTableComponent,
-        protected resolver: ComponentFactoryResolver,
-        protected injector: Injector) {
-        super(table, resolver, injector);
+  constructor(
+    @Inject(forwardRef(() => OTableComponent)) public table: OTableComponent,
+    protected resolver: ComponentFactoryResolver,
+    protected injector: Injector) {
+    super(table, resolver, injector);
 
-    }
+  }
 
 }
