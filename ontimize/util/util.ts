1c5aa00c9763c4738a415c55c15e9ad2b8057565
gonzalo.martinez <gonzalo.martinez@imatia.com>
2018-03-08 11:50:27 +0100

    Adding utility methods

--- ontimize/util/util.ts
+++ ontimize/util/util.ts
@@ -49,7 +49,7 @@ export class Util {
       return true;
     } else if ((typeof value === 'string') && (value.toUpperCase() === 'FALSE' || value.toUpperCase() === 'NO')) {
       return false;
-    } else if (defaultValue !== undefined && defaultValue !== null) {
+    } else if (Util.isDefined(defaultValue)) {
       return defaultValue;
     }
     return false;
@@ -171,5 +171,55 @@ export class Util {
     return true;
   }
 
+  static equals(o1: any, o2: any): boolean {
+    if (o1 === o2) {
+      return true;
+    }
+    if (o1 === null || o2 === null) {
+      return false;
+    }
+    if (o1 !== o1 && o2 !== o2) {
+      // NaN === NaN
+      return true;
+    }
+    let t1 = typeof o1, t2 = typeof o2, length: number, key: any, keySet: any;
+    if (t1 === t2 && t1 === 'object') {
+      if (Array.isArray(o1)) {
+        if (!Array.isArray(o2)) {
+          return false;
+        }
+        if ((length = o1.length) === o2.length) {
+          for (key = 0; key < length; key++) {
+            if (!Util.equals(o1[key], o2[key])) {
+              return false;
+            }
+          }
+          return true;
+        }
+      } else {
+        if (Array.isArray(o2)) {
+          return false;
+        }
+        keySet = Object.create(null);
+        for (key in o1) {
+          if (!Util.equals(o1[key], o2[key])) {
+            return false;
+          }
+          keySet[key] = true;
+        }
+        for (key in o2) {
+          if (!(key in keySet) && typeof o2[key] !== 'undefined') {
+            return false;
+          }
+        }
+        return true;
+      }
+    }
+    return false;
+  }
+
+  static isDefined(value: any): boolean {
+    return typeof value !== 'undefined' && value !== null;
+  }
 
 }
