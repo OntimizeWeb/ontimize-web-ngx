86ecd4f6432b85643a6d9bf3e65f300b997d5365
gonzalo.martinez <gonzalo.martinez@imatia.com>
2018-03-08 13:13:40 +0100

    Adding remote bundle loading

---
+++ ontimize/services/translate/o-translate-http-loader.ts
@@ -0,0 +1,93 @@
+import { Injector } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { TranslateHttpLoader } from '@ngx-translate/http-loader';
+import { Observable } from 'rxjs/Observable';
+import { AppConfig } from '../../config/app-config';
+
+export class OTranslateHttpLoader extends TranslateHttpLoader {
+  protected appConfig: AppConfig;
+  protected httpClient: HttpClient;
+
+  static BUNDLE_KEY = 'key';
+  static BUNDLE_VALUE = 'value';
+
+  constructor(
+    http: HttpClient,
+    prefix: string = '/assets/i18n/',
+    suffix: string = '.json',
+    protected injector: Injector
+  ) {
+
+    super(http, prefix, suffix);
+
+    this.httpClient = http;
+    this.appConfig = this.injector.get(AppConfig);
+  }
+
+  getLocalTranslation(lang: string): Observable<any> {
+    let innerObserver: any;
+    const dataObservable = new Observable(observer => innerObserver = observer).share();
+    super.getTranslation(lang)
+      .subscribe((res) => {
+        innerObserver.next(res);
+        innerObserver.complete();
+      }, error => {
+        innerObserver.next(undefined);
+      },
+      () => innerObserver.complete());
+    return dataObservable;
+  }
+
+  getTranslation(lang: string): any {
+    let translationOrigins: any[] = [];
+
+    translationOrigins.push(this.getLocalTranslation(lang));
+
+    if (this.appConfig.useRemoteBundle()) {
+      translationOrigins.push(this.getRemoteBundle(lang));
+    }
+
+    let innerObserver: any;
+    const dataObservable = new Observable(observer => innerObserver = observer).share();
+
+    Observable.combineLatest(...translationOrigins)
+      .subscribe((res: any[]) => {
+        const staticBundle = res[0] || {};
+        const remoteBundle = res[1] || {};
+        const allBundles = Object.assign(staticBundle, remoteBundle);
+        innerObserver.next(allBundles);
+      });
+    return dataObservable;
+  }
+
+  getRemoteBundle(lang: string): Observable<any> {
+    const bundleEndpoint = this.appConfig.getBundleEndpoint();
+    let innerObserver: any;
+    let dataObservable = new Observable(observer => innerObserver = observer).share();
+    if (!bundleEndpoint) {
+      innerObserver.next([]);
+    }
+    const url = bundleEndpoint + '?lang=' + lang;
+
+    this.httpClient.get(url).subscribe((resp: any) => {
+      let response = {};
+      if (resp.code === 0) {
+        response = this.parseBundleResponse(resp.data);
+      }
+      innerObserver.next(response);
+    },
+      error => innerObserver.next(error),
+      () => innerObserver.complete()
+    );
+
+    return dataObservable;
+  }
+
+  protected parseBundleResponse(data: any[]): any {
+    let result = {};
+    data.forEach((item) => {
+      result[item[OTranslateHttpLoader.BUNDLE_KEY]] = item[OTranslateHttpLoader.BUNDLE_VALUE];
+    });
+    return result;
+  }
+}
--- ontimize/services/translate/o-translate.service.ts
+++ ontimize/services/translate/o-translate.service.ts
@@ -1,12 +1,13 @@
 import { Injector, Injectable } from '@angular/core';
 import { EventEmitter } from '@angular/core';
 import { Http } from '@angular/http';
-import { TranslateService } from '@ngx-translate/core';
-import { MomentService, DialogService } from '../services';
-import * as CORE_TRANSLATIONS from '../i18n/i18n';
-import { ObservableWrapper } from '../util/async';
 import { Observable } from 'rxjs/Observable';
 import { Subscriber } from 'rxjs/Subscriber';
+import { TranslateService } from '@ngx-translate/core';
+import { MomentService, DialogService } from '../../services';
+import * as CORE_TRANSLATIONS from '../../i18n/i18n';
+import { ObservableWrapper } from '../../util/async';
+import { AppConfig } from '../../config/app-config';
 
 @Injectable()
 export class OTranslateService {
@@ -22,18 +23,32 @@ export class OTranslateService {
   protected http: Http;
 
   protected notFoundLang: Array<String> = [];
+  // protected _config: Config;
+  protected appConfig: AppConfig;
+
+  protected existingLangFiles: Array<String> = [];
 
   constructor(protected injector: Injector) {
     this.ngxTranslateService = this.injector.get(TranslateService);
     this.momentService = this.injector.get(MomentService);
     this.http = this.injector.get(Http);
+    this.appConfig = this.injector.get(AppConfig);
+    // this._config = this.injector.get(AppConfig).getConfiguration();
+
   }
 
   protected checkExistingLangFile(lang: string): Promise<any> {
     var self = this;
     return new Promise((resolve) => {
+      if (self.existingLangFiles.indexOf(lang) !== -1) {
+        resolve(true);
+        return;
+      }
       self.http.get(OTranslateService.ASSETS_PATH + lang + OTranslateService.ASSETS_EXTENSION)
         .subscribe(function () {
+          if (self.existingLangFiles.indexOf(lang) === -1) {
+            self.existingLangFiles.push(lang);
+          }
           // I18N File loaded successfully
           resolve(true);
         }, function () {
@@ -49,30 +64,21 @@ export class OTranslateService {
   public setDefaultLang(lang: string): void {
     this.checkExistingLangFile(lang).then((exists) => {
       if (exists) {
-        this.ngxTranslateService.setDefaultLang(lang);
+        // this.ngxTranslateService.setDefaultLang(lang);
+        this.ngxTranslateService.defaultLang = lang;
       }
     });
   }
 
-  public get(text: string): string {
-    let textTranslated = undefined;
+  public get(text: string, values: any[] = []): string {
+    let textTranslated = text;
     try {
-      let bundle = this.ngxTranslateService.get(text);
+      let bundle = this.ngxTranslateService.get(text, values);
       if (bundle && bundle['value']) {
         textTranslated = bundle['value'];
       }
-
-      textTranslated = textTranslated === text ? undefined : textTranslated;
     } catch (e) {
-      textTranslated = undefined;
-    }
-    if (!textTranslated) {
-      let bundle = CORE_TRANSLATIONS.MAP[this.ngxTranslateService.currentLang || this.DEFAULT_LANG];
-      if (bundle && bundle[text]) {
-        textTranslated = bundle[text];
-      } else {
-        textTranslated = text;
-      }
+      textTranslated = text;
     }
     return textTranslated;
   }
@@ -92,13 +98,13 @@ export class OTranslateService {
     } else {
       this.checkExistingLangFile(lang).then((exists) => {
         let newLang = lang;
-        if (!exists) {
+        if (!exists && !this.appConfig.useRemoteBundle()) {
           newLang = this.ngxTranslateService.getDefaultLang();
           this.injector.get(DialogService).alert('ERROR', CORE_TRANSLATIONS.MAP[newLang || this.DEFAULT_LANG]['MESSAGES.ERROR_MISSING_LANG']);
         }
         this.ngxTranslateService.use(newLang).subscribe(
           res => {
-            this.propagateLang(lang, res, observer);
+            this.propagateLang(newLang, res, observer);
           }
         );
       });
@@ -106,6 +112,11 @@ export class OTranslateService {
   }
 
   protected propagateLang(lang: string, langRes?: any, observer?: Subscriber<any>) {
+    const coreBundle = CORE_TRANSLATIONS.MAP[lang || this.DEFAULT_LANG];
+    if (coreBundle !== undefined) {
+      const mixed = Object.assign({}, coreBundle, langRes);
+      this.ngxTranslateService.translations[lang] = mixed;
+    }
     this.momentService.load(lang);
     ObservableWrapper.callEmit(this.onLanguageChanged, lang);
     if (observer) {
