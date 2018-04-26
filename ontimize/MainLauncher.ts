42dea41ca6cc6372649ddfa6ed1c890def558f97
gonzalo.martinez <gonzalo.martinez@imatia.com>
2018-04-26 15:41:49 +0200

    Protecting code

--- ontimize/MainLauncher.ts
+++ ontimize/MainLauncher.ts
@@ -16,8 +16,8 @@ export function ontimizeBootstrap(appModule: any, config?: any): Promise<NgModul
 
 export function ontimizePostBootstrap(ngModuleRef: NgModuleRef<any>): NgModuleRef<any> {
   // Hiding loader...
-  let loader: HTMLElement = document.getElementById('loader-wrapper');
-  if (loader) {
+  let loader: HTMLElement = document && document.getElementById('loader-wrapper');
+  if (loader && loader.parentNode) {
     loader.parentNode.removeChild(loader);
   }
   return ngModuleRef;
