0a04ce3321639897c2f91db5019bdadf6a062177
manu-cr <manuelcidrodriguez@gmail.com>
2018-07-18 17:18:52 +0200

    Add `oTabGroup` directive

--- ontimize/config/o-directives.ts
+++ ontimize/config/o-directives.ts
@@ -1,8 +1,10 @@
-import { OListItemDirective } from '../components/list/list-item/o-list-item.directive';
+import { OTabGroupDirective } from '../directives/o-tab-group.directive';
 import { OKeyboardListenerDirective } from '../directives/keyboard-listener.directive';
+import { OListItemDirective } from '../components/list/list-item/o-list-item.directive';
 
 export const ONTIMIZE_DIRECTIVES = [
-  //Ontimize directives...
+  // Ontimize directives...
+  OKeyboardListenerDirective,
   OListItemDirective,
-  OKeyboardListenerDirective
+  OTabGroupDirective
 ];
