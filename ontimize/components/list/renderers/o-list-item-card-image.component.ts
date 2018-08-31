255850b6f095a22df66d9de0a6fbbdf4aeb0356d
manu-cr <manuelcidrodriguez@gmail.com>
2018-08-31 18:27:36 +0200

    Update styles

--- ontimize/components/list/renderers/o-list-item-card-image.component.ts
+++ ontimize/components/list/renderers/o-list-item-card-image.component.ts
@@ -34,7 +34,6 @@ export const DEFAULT_OUTPUTS_O_LIST_ITEM_CARD_IMAGE = [
   ],
   encapsulation: ViewEncapsulation.None,
   host: {
-    'layout-padding': '',
     '[class.o-custom-list-item]': 'true',
     '[class.o-list-item-card-image]': 'true'
   }
