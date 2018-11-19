dc0d408762d77d566f6230db2a2744b329445fd9
gonzalo.martinez <gonzalo.martinez@imatia.com>
2018-11-19 13:55:56 +0100

    New o-app-layout-header component

---
+++ ontimize/layouts/app-layout/app-layout-header/o-app-layout-header.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  moduleId: module.id,
+  selector: 'o-app-layout-header',
+  templateUrl: './o-app-layout-header.component.html',
+})
+export class OAppLayoutHeaderComponent {
+
+}
