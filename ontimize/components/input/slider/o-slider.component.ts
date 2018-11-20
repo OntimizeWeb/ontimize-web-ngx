c9bc9365487e17716a9143f74b2d47bf35c882de
Patricia Martinez <patricia.martinez@imatia.com>
2018-11-20 10:36:40 +0100

    New property 'displayWith' in o-slider

--- ontimize/components/input/slider/o-slider.component.ts
+++ ontimize/components/input/slider/o-slider.component.ts
@@ -1,4 +1,4 @@
-import { Component, NgModule, Optional, forwardRef, Inject, Injector, ElementRef, ViewEncapsulation, Input } from '@angular/core';
+import { Component, NgModule, Optional, forwardRef, Inject, Injector, ElementRef, ViewEncapsulation } from '@angular/core';
 import { OFormDataComponent, DEFAULT_INPUTS_O_FORM_DATA_COMPONENT, DEFAULT_OUTPUTS_O_FORM_DATA_COMPONENT, OValueChangeEvent } from '../../o-form-data-component.class';
 import { CommonModule } from '@angular/common';
 import { OSharedModule } from '../../../shared';
@@ -17,13 +17,15 @@ export const DEFAULT_INPUTS_O_SLIDER_INPUT = [
   'step',
   'thumbLabel:thumb-label',
   'tickInterval:tick-interval',
-  'vertical'
+  'vertical',
+  'oDisplayWith:displayWith'
 ];
 
 export const DEFAULT_OUTPUTS_O_SLIDER_INPUT = [
   ...DEFAULT_OUTPUTS_O_FORM_DATA_COMPONENT
 ];
 
+export type SliderDisplayFunction = (value: number | null) => string | number;
 
 @Component({
   moduleId: module.id,
@@ -37,7 +39,6 @@ export const DEFAULT_OUTPUTS_O_SLIDER_INPUT = [
   outputs: DEFAULT_OUTPUTS_O_SLIDER_INPUT,
   encapsulation: ViewEncapsulation.None
 })
-
 export class OSliderComponent extends OFormDataComponent {
   public static DEFAULT_INPUTS_O_SLIDER_INPUT = DEFAULT_INPUTS_O_SLIDER_INPUT;
   public static DEFAULT_OUTPUTS_O_SLIDER_INPUT = DEFAULT_OUTPUTS_O_SLIDER_INPUT;
@@ -75,6 +76,16 @@ export class OSliderComponent extends OFormDataComponent {
   }
   get tickInterval() {
     return this._tickInterval;
+  }
+
+  protected _displayWith: SliderDisplayFunction;
+
+  get oDisplayWith(): SliderDisplayFunction {
+    return this._displayWith;
+  }
+
+  set oDisplayWith(val: SliderDisplayFunction) {
+    this._displayWith = val;
   }
 
   innerOnChange(event: any) {
