9ec0fa6dd20853046c359034924c6277bd17b6a2
gonzalo.martinez <gonzalo.martinez@imatia.com>
2018-12-14 12:11:27 +0100

    Updating moment files

--- ontimize/pipes/o-moment.pipe.ts
+++ ontimize/pipes/o-moment.pipe.ts
@@ -1,28 +1,25 @@
 
 import { Pipe, PipeTransform, Injector } from '@angular/core';
-import {
-    MomentService
-} from '../services';
-
+import { MomentService } from '../services';
 
 export interface IMomentPipeArgument {
-    format?: string;
+  format?: string;
 }
 
 @Pipe({
   name: 'oMoment'
 })
 
-
 export class OMomentPipe implements PipeTransform {
 
-    protected momentService: MomentService;
+  protected momentService: MomentService;
 
-    constructor(protected injector: Injector) {
-        this.momentService = this.injector.get(MomentService);
-    }
-    transform(value: any, args: IMomentPipeArgument) {
-        let format= args.format;
-        return this.momentService.parseDate(value, format);
-    }
+  constructor(protected injector: Injector) {
+    this.momentService = this.injector.get(MomentService);
+  }
+
+  transform(value: any, args: IMomentPipeArgument) {
+    let format = args.format;
+    return this.momentService.parseDate(value, format);
+  }
 }
