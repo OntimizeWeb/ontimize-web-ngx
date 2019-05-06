4b3b20b69976debff20a133fb210f55998399a38
Manuel Cid <manuel.cid@imatia.com>
2019-05-06 18:30:33 +0200

    Remove unused code

--- ontimize/layouts/card-menu-layout/o-card-menu-layout.component.ts
+++ ontimize/layouts/card-menu-layout/o-card-menu-layout.component.ts
@@ -1,12 +1,11 @@
-import { AfterViewInit, AfterViewChecked, ChangeDetectionStrategy, ChangeDetectorRef, Component, Injector, NgModule, OnDestroy, ViewEncapsulation } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { MediaChange, ObservableMedia } from '@angular/flex-layout';
+import { AfterViewInit, ChangeDetectionStrategy, ChangeDetectorRef, Component, Injector, NgModule, OnDestroy, ViewEncapsulation } from '@angular/core';
 import { Subscription } from 'rxjs';
 
-import { OSharedModule } from '../../shared/shared.module';
+import { OCardMenuItemModule } from '../../components/card-menu-item/o-card-menu-item.component';
 import { AppMenuService, OTranslateService } from '../../services';
 import { MenuGroup, MenuRootItem } from '../../services/app-menu.service';
-import { OCardMenuItemModule } from '../../components/card-menu-item/o-card-menu-item.component';
+import { OSharedModule } from '../../shared/shared.module';
 
 export const DEFAULT_INPUTS_O_MENU_LAYOUT = [
   'parentMenuId : parent-menu-id'
@@ -28,13 +27,11 @@ export const DEFAULT_OUTPUTS_O_MENU_LAYOUT = [
   },
   changeDetection: ChangeDetectionStrategy.OnPush
 })
-export class OCardMenuLayoutComponent implements AfterViewChecked, AfterViewInit, OnDestroy {
+export class OCardMenuLayoutComponent implements AfterViewInit, OnDestroy {
 
   public static DEFAULT_INPUTS_O_MENU_LAYOUT = DEFAULT_INPUTS_O_MENU_LAYOUT;
   public static DEFAULT_OUTPUTS_O_MENU_LAYOUT = DEFAULT_OUTPUTS_O_MENU_LAYOUT;
 
-  protected mediaWatch: Subscription;
-  protected media: ObservableMedia;
   protected translateService: OTranslateService;
   protected translateServiceSubscription: Subscription;
   protected appMenuService: AppMenuService;
@@ -42,20 +39,10 @@ export class OCardMenuLayoutComponent implements AfterViewChecked, AfterViewInit
   protected cardItemsArray: MenuRootItem[];
   protected parentMenuId: string;
 
-  get isSmallScreen(): boolean {
-    return this._isSmallScreen;
-  }
-  set isSmallScreen(val: boolean) {
-    this._isSmallScreen = val;
-    this.cd.detectChanges();
-  }
-  protected _isSmallScreen: boolean = false;
-
   constructor(
     private injector: Injector,
     private cd: ChangeDetectorRef
   ) {
-    this.media = this.injector.get(ObservableMedia);
     this.translateService = this.injector.get(OTranslateService);
     this.appMenuService = this.injector.get(AppMenuService);
     this.menuRoots = this.appMenuService.getMenuRoots();
@@ -65,34 +52,20 @@ export class OCardMenuLayoutComponent implements AfterViewChecked, AfterViewInit
     });
   }
 
-  ngAfterViewInit() {
+  public ngAfterViewInit(): void {
     this.setCardMenuItems();
   }
 
-  ngAfterViewChecked(): void {
-    this.mediaWatch = this.media.subscribe((change: MediaChange) => {
-      if (['xs', 'sm'].indexOf(change.mqAlias) !== -1) {
-        this.isSmallScreen = true;
-      }
-      if (['md', 'lg', 'xl'].indexOf(change.mqAlias) !== -1) {
-        this.isSmallScreen = false;
-      }
-    });
-  }
-
-  ngOnDestroy() {
-    if (this.mediaWatch) {
-      this.mediaWatch.unsubscribe();
-    }
+  public ngOnDestroy(): void {
     if (this.translateServiceSubscription) {
       this.translateServiceSubscription.unsubscribe();
     }
   }
 
-  setCardMenuItems() {
+  public setCardMenuItems(): void {
     let cardItemsAux = [];
     if (!this.parentMenuId) {
-      cardItemsAux = this.menuRoots.filter((item) => !this.appMenuService.isMenuGroup(item));
+      cardItemsAux = this.menuRoots.filter(item => !this.appMenuService.isMenuGroup(item));
     } else {
       cardItemsAux = this.getItemsFilteredByParentId(this.menuRoots);
     }
@@ -110,8 +83,8 @@ export class OCardMenuLayoutComponent implements AfterViewChecked, AfterViewInit
   }
 
   protected getItemsFilteredByParentId(array: MenuRootItem[]): MenuRootItem[] {
-    let result: MenuRootItem[] = undefined;
-    const groups = array.filter((item) => this.appMenuService.isMenuGroup(item));
+    let result: MenuRootItem[];
+    const groups = array.filter(item => this.appMenuService.isMenuGroup(item));
 
     for (let i = 0, len = groups.length; i < len; i++) {
       const menuGroup = (groups[i] as MenuGroup);
