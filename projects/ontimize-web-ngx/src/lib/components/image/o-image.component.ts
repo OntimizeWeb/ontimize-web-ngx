08435ca10fc4dc76d978f3b4d704cb33b495f65d
Paco Gomez Imatia <francisco.gomez@imatia.com>
2020-07-22 11:18:56 +0200

    Added ngOnDestroy

--- projects/ontimize-web-ngx/src/lib/components/image/o-image.component.ts
+++ projects/ontimize-web-ngx/src/lib/components/image/o-image.component.ts
@@ -118,6 +118,10 @@ export class OImageComponent extends OFormDataComponent implements OnInit, OnDes
     }
   }
 
+  public ngOnDestroy(): void {	
+    super.ngOnDestroy();	
+  }
+
   public ensureOFormValue(val: any): void {
     if (val instanceof OFormValue) {
       if (val.value && val.value.bytes !== undefined) {
