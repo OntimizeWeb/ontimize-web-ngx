bc22be8e24f05f5123dc52f6ea70eb9198b2151a
Patricia Martinez <patricia.martinez@imatia.com>
2020-09-09 09:16:57 +0200

    Fixing reloadData when clear column filters

--- projects/ontimize-web-ngx/src/lib/components/table/o-table.component.ts
+++ projects/ontimize-web-ngx/src/lib/components/table/o-table.component.ts
@@ -1715,6 +1715,11 @@ export class OTableComponent extends OServiceComponent implements OnInit, OnDest
     }
   }
 
+  clearColumnFilters(triggerDatasourceUpdate: boolean = true): void {
+    this.dataSource.clearColumnFilters(triggerDatasourceUpdate);
+    this.reloadPaginatedDataFromStart();
+  }
+
   isColumnFilterable(column: OColumn): boolean {
     return (this.oTableColumnsFilterComponent && this.oTableColumnsFilterComponent.isColumnFilterable(column.attr));
   }
