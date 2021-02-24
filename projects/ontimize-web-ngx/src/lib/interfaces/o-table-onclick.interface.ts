f035953366b63c87804a514d5147b2360d828a80
Patricia Martinez <patricia.martinez@imatia.com>
2021-02-24 16:51:51 +0100

    Added columnId and columnIndex property in OnClickTableEvent interface

--- projects/ontimize-web-ngx/src/lib/interfaces/o-table-onclick.interface.ts
+++ projects/ontimize-web-ngx/src/lib/interfaces/o-table-onclick.interface.ts
@@ -3,5 +3,10 @@ export interface OnClickTableEvent {
   row: any;
   /** row index */
   rowIndex: number;
+  /** mouseEvent */
   mouseEvent: MouseEvent;
+  /**attr column   */
+  columnId: string;
+  /** column index in visible columns */
+  columnIndex: number;
 }
