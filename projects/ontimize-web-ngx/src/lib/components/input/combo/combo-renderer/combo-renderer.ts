2d20d53b2042640866af50dae563c4654159f426
Paco Gomez Imatia <francisco.gomez@imatia.com>
2021-03-11 16:22:38 +0100

    Date, Integer and Percentage renderers

--- projects/ontimize-web-ngx/src/lib/components/input/combo/combo-renderer/combo-renderer.ts
+++ projects/ontimize-web-ngx/src/lib/components/input/combo/combo-renderer/combo-renderer.ts
@@ -1,30 +1,34 @@
 // import { DEFAULT_OUTPUTS_O_TABLE_CELL_RENDERER_ACTION } from './action/o-table-cell-renderer-action.component';
 import {
+  DEFAULT_INPUTS_O_COMBO_RENDERER_BOOLEAN,
+  OComboRendererBooleanComponent
+} from './boolean/o-combo-renderer-boolean.component';
+import {
   DEFAULT_INPUTS_O_COMBO_RENDERER_CURRENCY,
   OComboRendererCurrencyComponent
 } from './currency/o-combo-renderer-currency.component';
+import { DEFAULT_INPUTS_O_COMBO_RENDERER_DATE, OComboRendererDateComponent } from './date/o-combo-renderer-date.component';
 import { OComboRendererIntegerComponent } from './integer/o-combo-renderer-integer.component';
+import {
+  DEFAULT_INPUTS_O_COMBO_RENDERER_PERCENTAGE,
+  OComboRendererPercentageComponent
+} from './percentage/o-combo-renderer-percentage.component';
 import { OComboRendererRealComponent } from './real/o-combo-renderer-real.component';
 
-// import { OTableCellRendererBooleanComponent } from './boolean/o-table-cell-renderer-boolean.component';
-// import { OTableCellRendererDateComponent } from './date/o-table-cell-renderer-date.component';
+
 // import { OTableCellRendererImageComponent } from './image/o-table-cell-renderer-image.component';
-// import { OTableCellRendererIntegerComponent } from './integer/o-table-cell-renderer-integer.component';
-// import { OTableCellRendererPercentageComponent } from './percentage/o-table-cell-renderer-percentage.component';
-// import { OTableCellRendererRealComponent } from './real/o-table-cell-renderer-real.component';
 // import { OTableCellRendererServiceComponent } from './service/o-table-cell-renderer-service.component';
 // import { OTableCellRendererTimeComponent } from './time/o-table-cell-renderer-time.component';
 // import { OTableCellRendererTranslateComponent } from './translate/o-table-cell-renderer-translate.component';
 
 export const O_COMBO_RENDERERS = [
-  // OTableCellRendererActionComponent,
-  // OTableCellRendererDateComponent,
-  // OTableCellRendererBooleanComponent,
-  // OTableCellRendererImageComponent,
+  OComboRendererBooleanComponent,
   OComboRendererIntegerComponent,
   OComboRendererRealComponent,
   OComboRendererCurrencyComponent,
-  // OTableCellRendererPercentageComponent,
+  OComboRendererDateComponent,
+  OComboRendererPercentageComponent,
+  // OTableCellRendererActionComponent,
   // OTableCellRendererServiceComponent,
   // OTableCellRendererTranslateComponent,
   // OTableCellRendererTimeComponent
@@ -32,6 +36,9 @@ export const O_COMBO_RENDERERS = [
 
 export const O_COMBO_RENDERERS_INPUTS = [
   ...DEFAULT_INPUTS_O_COMBO_RENDERER_CURRENCY, // includes Integer and Real
+  ...DEFAULT_INPUTS_O_COMBO_RENDERER_BOOLEAN,
+  ...DEFAULT_INPUTS_O_COMBO_RENDERER_DATE,
+  ...DEFAULT_INPUTS_O_COMBO_RENDERER_PERCENTAGE
 ];
 
 export const O_COMBO_RENDERERS_OUTPUTS = [
@@ -41,11 +48,10 @@ export const renderersMapping = {
   integer: OComboRendererIntegerComponent,
   real: OComboRendererRealComponent,
   currency: OComboRendererCurrencyComponent,
+  boolean: OComboRendererBooleanComponent,
+  date: OComboRendererDateComponent,
+  percentage: OComboRendererPercentageComponent,
   // action: OTableCellRendererActionComponent,
-  // boolean: OTableCellRendererBooleanComponent,
-  // date: OTableCellRendererDateComponent,
-  // image: OTableCellRendererImageComponent,
-  // percentage: OTableCellRendererPercentageComponent,
   // service: OTableCellRendererServiceComponent,
   // translate: OTableCellRendererTranslateComponent,
   // time: OTableCellRendererTimeComponent
