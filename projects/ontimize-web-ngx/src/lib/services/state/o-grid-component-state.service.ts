17a3263bd12da4b1f8b81d8d781065ea147cab03
gonzalo-martinez-imt <gonzalo.martinez@imatia.com>
2021-06-18 11:29:12 +0200

    Issue #640

--- projects/ontimize-web-ngx/src/lib/services/state/o-grid-component-state.service.ts
+++ projects/ontimize-web-ngx/src/lib/services/state/o-grid-component-state.service.ts
@@ -6,7 +6,6 @@ import { Util } from '../../util/util';
 import { AbstractComponentStateService } from './o-component-state.service';
 import { OGridComponentStateClass } from './o-grid-component-state.class';
 
-
 @Injectable()
 export class OGridComponentStateService extends AbstractComponentStateService<OGridComponentStateClass, OGridComponent> {
 
@@ -35,6 +34,10 @@ export class OGridComponentStateService extends AbstractComponentStateService<OG
     if (Util.isDefined(this.component.sortColumnOrder)) {
       dataToStore['sort-column'] = this.component.sortColumnOrder.columnName + Codes.COLUMNS_ALIAS_SEPARATOR +
         (this.component.sortColumnOrder.ascendent ? Codes.ASC_SORT : Codes.DESC_SORT);
+    }
+
+    if (this.component.quickFilter && Util.isDefined(this.component.quickFilterComponent)) {
+      dataToStore['quickFilterActiveColumns'] = this.component.quickFilterComponent.getActiveColumns().join(Codes.ARRAY_INPUT_SEPARATOR);
     }
     return dataToStore;
   }
