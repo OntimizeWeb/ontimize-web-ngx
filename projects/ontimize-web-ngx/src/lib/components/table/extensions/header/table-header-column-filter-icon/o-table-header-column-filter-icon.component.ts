4ee1475f66a12cdf15d19576cbc07298cae37302
IMATIASL\patricia.martinez <patricia.martinez@imatia.com>
2021-06-24 16:33:09 +0200

    Show filter icon when the mouse make hover in header

--- projects/ontimize-web-ngx/src/lib/components/table/extensions/header/table-header-column-filter-icon/o-table-header-column-filter-icon.component.ts
+++ projects/ontimize-web-ngx/src/lib/components/table/extensions/header/table-header-column-filter-icon/o-table-header-column-filter-icon.component.ts
@@ -1,4 +1,6 @@
+import { animate, state, style, transition, trigger } from '@angular/animations';
 import { ChangeDetectionStrategy, Component, forwardRef, Inject, OnDestroy, OnInit, ViewEncapsulation } from '@angular/core';
+import { AnimationDurations } from '@angular/material';
 import { BehaviorSubject, Subscription } from 'rxjs';
 
 import { OColumnValueFilter } from '../../../../../types/o-column-value-filter.type';
@@ -18,8 +20,19 @@ export const DEFAULT_INPUTS_O_TABLE_COLUMN_FILTER_ICON = [
   encapsulation: ViewEncapsulation.None,
   changeDetection: ChangeDetectionStrategy.OnPush,
   host: {
-    '[class.o-table-column-filter-icon]': 'true'
-  }
+    '[class.o-table-column-filter-icon]': 'true',
+    '(mouseenter)': 'setFilterIconHintVisible(true)',
+    '(mouseleave)': 'setFilterIconHintVisible(false)'
+  },
+  animations: [
+    trigger('iconState', [
+      state('ACTIVE, HINT', style({ opacity: 1 })),
+      state('INACTIVE', style({ opacity: 0 })),
+      //transition('* <=> INACTIVE', animate(AnimationDurations.EXITING)),
+      transition('* <=> HINT', animate(AnimationDurations.ENTERING))
+    ]),
+
+  ]
 })
 export class OTableHeaderColumnFilterIconComponent implements OnInit, OnDestroy {
 
@@ -28,6 +41,7 @@ export class OTableHeaderColumnFilterIconComponent implements OnInit, OnDestroy 
   public isColumnFilterActive: BehaviorSubject<boolean> = new BehaviorSubject(false);
   public indicatorNumber: BehaviorSubject<string> = new BehaviorSubject('');
   private subscription = new Subscription();
+  public filterIconStateView: 'HINT' | 'ACTIVE' | 'INACTIVE' = 'ACTIVE';
 
   constructor(
     @Inject(forwardRef(() => OTableComponent)) public table: OTableComponent
@@ -43,7 +57,9 @@ export class OTableHeaderColumnFilterIconComponent implements OnInit, OnDestroy 
 
   public updateStateColumnFilter() {
     this.indicatorNumber.next(this.getFilterIndicatorNumbered());
+
     this.isColumnFilterActive.next(Util.isDefined(this.getColumnValueFilterByAttr()));
+    this.filterIconStateView = this.isColumnFilterActive.getValue() ? 'ACTIVE' : 'INACTIVE';
   }
 
   protected getColumnValueFilterByAttr(): OColumnValueFilter {
@@ -70,6 +86,18 @@ export class OTableHeaderColumnFilterIconComponent implements OnInit, OnDestroy 
     }
 
     return result;
+  }
+
+  /**
+   * Sets the "hint" state such that the icon will be semi-transparently displayed as a hint to the
+   * user showing what the active filter by column will become. If set to false, the icon will fade away.
+   */
+  setFilterIconHintVisible(visible: boolean) {
+    // No-op if the sort header is disabled - should not make the hint visible.
+    if (this.filterIconStateView === 'ACTIVE') { return; }
+    this.filterIconStateView = visible ? 'HINT' : 'INACTIVE';
+
+
   }
 
   ngOnDestroy(): void {
