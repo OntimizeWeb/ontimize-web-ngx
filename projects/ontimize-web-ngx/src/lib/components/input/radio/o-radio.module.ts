169262fed6f7890123162e11df94a13451873dff
IMATIASL\patricia.martinez <patricia.martinez@imatia.com>
2021-08-11 10:36:09 +0200

    Wrong import of OContextMenuModule

--- projects/ontimize-web-ngx/src/lib/components/input/radio/o-radio.module.ts
+++ projects/ontimize-web-ngx/src/lib/components/input/radio/o-radio.module.ts
@@ -2,7 +2,7 @@ import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 
 import { OSharedModule } from '../../../shared/shared.module';
-import { OContextMenuModule } from '../../contextmenu';
+import { OContextMenuModule } from '../../contextmenu/o-context-menu.module';
 import { ORadioComponent } from './o-radio.component';
 
 @NgModule({
