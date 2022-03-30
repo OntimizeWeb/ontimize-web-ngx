22ecca4ff5bb10bda8c54d97f3c2919f98888d7c
IMATIASL\patricia.martinez <patricia.martinez@imatia.com>
2022-03-30 14:16:22 +0200

    Updated oSafe pipe

--- projects/ontimize-web-ngx/src/lib/pipes/o-safe.pipe.ts
+++ projects/ontimize-web-ngx/src/lib/pipes/o-safe.pipe.ts
@@ -1,5 +1,5 @@
 import { Injector, Pipe, PipeTransform, Type } from '@angular/core';
-import { DomSanitizer, SafeHtml, SafeStyle, SafeScript, SafeUrl, SafeResourceUrl } from '@angular/platform-browser';
+import { DomSanitizer, SafeHtml, SafeUrl, SafeResourceUrl } from '@angular/platform-browser';
 
 @Pipe({
   name: 'oSafe'
@@ -8,9 +8,9 @@ export class OSafePipe implements PipeTransform {
   protected sanitizer: DomSanitizer;
   constructor(protected injector: Injector) {
     this.sanitizer = this.injector.get<DomSanitizer>(DomSanitizer as Type<DomSanitizer>);
-   }
+  }
 
-  public transform(value: any, type: string): SafeHtml | SafeStyle | SafeScript | SafeUrl | SafeResourceUrl {
+  public transform(value: any, type: string): SafeHtml | SafeUrl | SafeResourceUrl {
     switch (type) {
       case 'html': return this.sanitizer.bypassSecurityTrustHtml(value);
       case 'url': return this.sanitizer.bypassSecurityTrustUrl(value);
