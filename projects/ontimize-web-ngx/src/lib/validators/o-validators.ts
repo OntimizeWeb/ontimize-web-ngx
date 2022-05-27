594a99443fbde7ab00a2078ea01a1a828c6b0df0
gonzalo-martinez-imt <gonzalo.martinez@imatia.com>
2022-05-27 08:41:45 +0200

    Updating validator

--- projects/ontimize-web-ngx/src/lib/validators/o-validators.ts
+++ projects/ontimize-web-ngx/src/lib/validators/o-validators.ts
@@ -49,62 +49,33 @@ export class OValidators {
   /**
    * Phone validator
    */
-  public static phoneValidator(control: any): ValidationErrors {
-    if (!control.value) {
-      return null;
+  public static phoneValidator(control: FormControl, countryCode?: any): ValidationErrors {
+    if (!control || !control.value) {
+      return {};
     }
-    // Find <input> inside injected nativeElement and get its "id".
-    const el: HTMLElement = control.nativeElement as HTMLElement;
-    const inputBox: HTMLInputElement = el
-      ? el.querySelector('input[type="tel"]')
-      : undefined;
-    if (inputBox) {
-      const id = inputBox.id;
-      const isCheckValidation = inputBox.getAttribute('validation');
-      if (isCheckValidation === 'true') {
-        const isRequired = control.errors && control.errors.required === true;
-        const error = { validatePhoneNumber: { valid: false } };
+    const phoneNumberUtilInstance = lpn.PhoneNumberUtil.getInstance();
+    const error = { validatePhoneNumber: { valid: false } };
 
-        inputBox.setCustomValidity('Invalid field.');
-
-        let number: lpn.PhoneNumber;
-
-        try {
-          number = lpn.PhoneNumberUtil.getInstance().parse(
-            control.value.number,
-            control.value.countryCode
-          );
-        } catch (e) {
-          if (isRequired === true) {
-            return error;
-          } else {
-            inputBox.setCustomValidity('');
-          }
-        }
-
-        if (control.value) {
-          if (!number) {
-            return error;
-          } else {
-            if (
-              !lpn.PhoneNumberUtil.getInstance().isValidNumberForRegion(
-                number,
-                control.value.countryCode
-              )
-            ) {
-              return error;
-            } else {
-              inputBox.setCustomValidity('');
-            }
-          }
-        }
-      } else if (isCheckValidation === 'false') {
-        inputBox.setCustomValidity('');
-
-        control.clearValidators();
+    let number: lpn.PhoneNumber;
+    let isValidForRegion: boolean = false
+    try {
+      number = phoneNumberUtilInstance.parse(
+        control.value,
+        countryCode
+      );
+      if (number) {
+        isValidForRegion = phoneNumberUtilInstance.isValidNumberForRegion(
+          number,
+          countryCode
+        )
       }
+    } catch (e) {
+      return error;
     }
-    return null;
+    if (control.value && (!number || !isValidForRegion)) {
+      return error
+    }
+    return {};
   }
 
   /**
