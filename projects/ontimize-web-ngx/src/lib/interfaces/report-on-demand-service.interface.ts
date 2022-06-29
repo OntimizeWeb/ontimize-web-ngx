625de39c01a95ae2fafa86d9e6449e5b4691414a
IMATIASL\patricia.martinez <patricia.martinez@imatia.com>
2022-06-29 15:47:50 +0200

    Fixes #986

--- projects/ontimize-web-ngx/src/lib/interfaces/report-on-demand-service.interface.ts
+++ projects/ontimize-web-ngx/src/lib/interfaces/report-on-demand-service.interface.ts
@@ -1,5 +1,6 @@
 import { OTableComponent } from "../components";
 
-export interface IReportOnDemandService {
-  openReportOnDemand(table: OTableComponent);
+export interface IReportService {
+  openReportOnDemand(table: OTableComponent): void;
+  openFillReport(reportId: string, parametersValues: object, filter: object): void
 }
