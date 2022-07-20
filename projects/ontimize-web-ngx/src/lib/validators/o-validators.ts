46c6e78eaed21930aeee1be5e1f35462cb2b8f4d
Tomas Fuentes <tomasfacal@gmail.com>
2022-07-20 12:38:05 +0200

    updated email pattern

--- projects/ontimize-web-ngx/src/lib/validators/o-validators.ts
+++ projects/ontimize-web-ngx/src/lib/validators/o-validators.ts
@@ -2,7 +2,7 @@ import { FormControl, ValidationErrors, ValidatorFn } from '@angular/forms';
 import { Util } from '../util';
 import * as lpn from 'google-libphonenumber';
 
-const EMAIL_REGEXP = /^(?:[a-zA-Z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-zA-Z0-9!#$%&'*+/=?^_`{|}~-]+)*|\x22(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*\x22)@(?:(?:[a-zA-Z0-9](?:[a-zA-Z0-9-]*[a-zA-Z0-9])?\.)+[a-zA-Z0-9](?:[a-zA-Z0-9-]*[a-zA-Z0-9])?|\[(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?|[a-zA-Z0-9-]*[a-zA-Z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\])$/;
+const EMAIL_REGEXP = /^[a-zA-Z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-zA-Z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-zA-Z0-9](?:[a-zA-Z0-9-]*[a-zA-Z0-9])?\.)+[a-zA-Z0-9](?:[a-zA-Z0-9-]*[a-zA-Z0-9])?$/;
 
 // NIF Regular Expressions
 const DNI_PATTERN = '^(([0-9]{8})([-]?)([a-zA-Z]{1}))$';
