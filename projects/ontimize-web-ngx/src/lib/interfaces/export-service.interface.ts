2eaa9c7f4f43a9e4671217d529997681825c4716
IMATIASL\patricia.martinez <patricia.martinez@imatia.com>
2022-08-11 12:21:26 +0200

    Updated IExportService interface

--- projects/ontimize-web-ngx/src/lib/interfaces/export-service.interface.ts
+++ projects/ontimize-web-ngx/src/lib/interfaces/export-service.interface.ts
@@ -3,5 +3,5 @@ import { Observable } from 'rxjs';
 export interface IExportService {
   getDefaultServiceConfiguration(serviceName?: string): any;
   configureService(config: any): void;
-  exportData(data: any, format: string): Observable<any>;
+  exportData(format: string): Observable<any>;
 }
