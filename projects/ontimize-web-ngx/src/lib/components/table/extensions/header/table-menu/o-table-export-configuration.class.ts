6a553858da97649bdd3ee0919cc13cdd3ed84cbe
IMATIASL\patricia.martinez <patricia.martinez@imatia.com>
2022-08-12 15:06:35 +0200

    Updated OTableExportConfiguration

--- projects/ontimize-web-ngx/src/lib/components/table/extensions/header/table-menu/o-table-export-configuration.class.ts
+++ projects/ontimize-web-ngx/src/lib/components/table/extensions/header/table-menu/o-table-export-configuration.class.ts
@@ -1,28 +1,6 @@
 export class OTableExportConfiguration {
-  columns: Array<any>;
-  columnNames: { [columnId: string]: string; };
-  sqlTypes: { [columnId: string]: number; };
   service: string;
   serviceType: string;
-  filter?: object;
-  entity: string;
   visibleButtons: string;
   options?: any;
-  advQuery: boolean = false;
-  offset?: number = -1;
-  pageSize?: number = 25;
-}
-
-export type OTableExportQueryParam = {
-  columns: string[],
-  sqltypes: { [key: string]: number; };
-}
-
-export type OTableExportColumnStyle = {
-  dataFormatString?: string;
-  alignment?: string;
-  verticalAlignment?: string;
-  fillBackgroundColor?: string;
-  width?: number;
-
 }
