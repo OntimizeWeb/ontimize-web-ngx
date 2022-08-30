0d18f85e5bf606df93945b19483b71350c1d790e
IMATIASL\patricia.martinez <patricia.martinez@imatia.com>
2022-08-30 14:52:40 +0200

    Fixed that the no results message was not displayed when the query is aborted

--- projects/ontimize-web-ngx/src/lib/components/o-service-base-component.class.ts
+++ projects/ontimize-web-ngx/src/lib/components/o-service-base-component.class.ts
@@ -385,6 +385,10 @@ export abstract class AbstractOServiceBaseComponent<T extends AbstractComponentS
         this.state.queryRecordOffset = 0;
         this.state.totalQueryRecordsNumber = 0;
         this.setData([], []);
+        /**  this.cd.detectChanges() is used to update loadingSubject value (this.loadingSubject.next(true); in line 377)
+         *  before using the next line and so update the oTableExpandedFooter directive and display the message
+         * that there are no results when the query is aborted*/
+        this.cd.detectChanges();
         this.loadingSubject.next(false);
         return;
       }
