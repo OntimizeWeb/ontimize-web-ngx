52e165452b5ccb8ef492b82b9984fa9eb2f710cf
IMATIASL\patricia.martinez <patricia.martinez@imatia.com>
2022-10-25 12:01:02 +0200

    Removed filter property in AdvancedQueryParameter

--- projects/ontimize-web-ngx/src/lib/interfaces/query-parameter.interface.ts
+++ projects/ontimize-web-ngx/src/lib/interfaces/query-parameter.interface.ts
@@ -5,11 +5,10 @@ import { SQLOrder } from "../types/sql-order.type";
 export interface QueryParameter  {
   columns: string[];
   sqltypes: { [key: string]: SQLTypes; };
-  filter: BasicExpression,
+  filter: BasicExpression;
 }
 export interface AdvancedQueryParameter extends QueryParameter  {
   offset: number;
   pageSize: number;
-  filter: BasicExpression,
-  orderBy: SQLOrder
+  orderBy: SQLOrder;
 }
