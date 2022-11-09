0bb91593feaca07e65210c19b20131d1cb3044ca
IMATIASL\patricia.martinez <patricia.martinez@imatia.com>
2022-11-09 16:18:41 +0100

    New type OFilterBuilderStatus

--- projects/ontimize-web-ngx/src/lib/types/o-filter-builder-values.type.ts
+++ projects/ontimize-web-ngx/src/lib/types/o-filter-builder-values.type.ts
@@ -2,3 +2,9 @@ export type OFilterBuilderValues = {
   attr: string,
   value: any;
 }
+
+export type OFilterBuilderStatus = {
+  name: string;
+  description: string;
+  'stored-filter': OFilterBuilderValues[];
+}
