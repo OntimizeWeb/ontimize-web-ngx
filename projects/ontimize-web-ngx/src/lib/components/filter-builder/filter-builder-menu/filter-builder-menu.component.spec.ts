7ec8f1b71ef845b224afd3684f45cba3aaa15ce9
IMATIASL\patricia.martinez <patricia.martinez@imatia.com>
2022-11-10 09:26:10 +0100

    Added spec about FilterBuilderQueryButtonComponent

--- projects/ontimize-web-ngx/src/lib/components/filter-builder/filter-builder-menu/filter-builder-menu.component.spec.ts
+++ projects/ontimize-web-ngx/src/lib/components/filter-builder/filter-builder-menu/filter-builder-menu.component.spec.ts
@@ -1,6 +1,14 @@
+import { Injector } from '@angular/core';
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { MatButtonModule, MatDialogModule, MatIconModule, MatMenuModule } from '@angular/material';
+import { TranslateService } from '@ngx-translate/core';
+import { AppConfig, APP_CONFIG } from '../../../config/app-config';
+import { OTranslateModule } from '../../../pipes/o-translate.pipe';
 import { OFilterBuilderMenuComponent } from './filter-builder-menu.component';
+import { appConfigFactory } from '../../../services';
+import { HttpClientModule } from '@angular/common/http';
+import { NoopAnimationsModule } from '@angular/platform-browser/animations';
+import { TestUtils } from '../../input/test/test-utils';
 
 describe('FilterBuilderQueryButtonComponent', () => {
   let component: OFilterBuilderMenuComponent;
@@ -8,9 +16,19 @@ describe('FilterBuilderQueryButtonComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [OFilterBuilderMenuComponent ]
+      declarations: [OFilterBuilderMenuComponent],
+      imports: [MatIconModule, OTranslateModule, MatDialogModule, MatButtonModule, MatMenuModule, HttpClientModule, NoopAnimationsModule],
+      providers: [
+        {
+          provide: TranslateService,
+          useClass: TranslateService,
+          deps: [Injector]
+        },
+        { provide: APP_CONFIG, useValue: TestUtils.mockConfiguration() },
+        { provide: AppConfig, useFactory: appConfigFactory, deps: [Injector] }
+      ]
     })
-    .compileComponents();
+      .compileComponents();
   }));
 
   beforeEach(() => {
