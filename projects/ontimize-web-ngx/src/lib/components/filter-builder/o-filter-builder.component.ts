42503f6ddd4ce590fd9ae7016b5b27ab0c09fa6b
Patricia Martinez <32088627+patricia-martinez-imatia@users.noreply.github.com>
2022-11-16 09:33:53 +0100

    `oFilterBuilderClear` directive not clearing the value of the `o-combo` multiple  (#1082)
    * Fixes #1081
    
    * Updated CHANGELOG.md

--- projects/ontimize-web-ngx/src/lib/components/filter-builder/o-filter-builder.component.ts
+++ projects/ontimize-web-ngx/src/lib/components/filter-builder/o-filter-builder.component.ts
@@ -198,7 +198,7 @@ export class OFilterBuilderComponent implements AfterViewInit, OnDestroy, OnInit
   clearFilter(): void {
     const formComponents = this.form.getComponents();
     this.getFilterAttrs().forEach((attr: string) => {
-      formComponents[attr].setValue(void 0);
+      formComponents[attr].clearValue();
     });
     this.onClear.emit();
   }
