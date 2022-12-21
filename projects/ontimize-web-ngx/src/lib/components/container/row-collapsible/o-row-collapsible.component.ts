47d11052da106dee2d4c2eabcfcc2d04b0e709b0
Patricia Martinez <32088627+patricia-martinez-imatia@users.noreply.github.com>
2022-12-21 08:50:59 +0100

    New outputs `onClosed, onOpened, onAfterCollapse, onAfterExpand` (#1113)
    * Fixes #1074
    
    * Resolving code smell
    
    * Updated CHANGELOG
    
    * Fixed typo

--- projects/ontimize-web-ngx/src/lib/components/container/row-collapsible/o-row-collapsible.component.ts
+++ projects/ontimize-web-ngx/src/lib/components/container/row-collapsible/o-row-collapsible.component.ts
@@ -3,6 +3,7 @@ import { MAT_FORM_FIELD_DEFAULT_OPTIONS } from '@angular/material';
 
 import {
   DEFAULT_INPUTS_O_CONTAINER_COLLAPSIBLE,
+  DEFAULT_OUTPUTS_O_CONTAINER_COLLAPSIBLE,
   OContainerCollapsibleComponent
 } from '../o-container-collapsible-component.class';
 
@@ -10,10 +11,15 @@ export const DEFAULT_INPUTS_O_ROW_COLLAPSIBLE = [
   ...DEFAULT_INPUTS_O_CONTAINER_COLLAPSIBLE
 ];
 
+export const DEFAULT_OUTPUTS_O_ROW_COLLAPSIBLE = [
+  ...DEFAULT_OUTPUTS_O_CONTAINER_COLLAPSIBLE
+];
+
 @Component({
   selector: 'o-row-collapsible',
   templateUrl: './o-row-collapsible.component.html',
   inputs: DEFAULT_INPUTS_O_ROW_COLLAPSIBLE,
+  outputs: DEFAULT_OUTPUTS_O_ROW_COLLAPSIBLE,
   encapsulation: ViewEncapsulation.None,
   host: {
     '[class.o-row-collapsible]': 'true',
