ea0bbc0ebd91c4ec31177122ec7a7dc40b1ca5ed
IMATIASL\patricia.martinez <patricia.martinez@imatia.com>
2023-01-20 10:23:17 +0100

    Resolved code smell

--- projects/ontimize-web-ngx/src/lib/util/util.ts
+++ projects/ontimize-web-ngx/src/lib/util/util.ts
@@ -497,8 +497,7 @@ export class Util {
   }
 
   static isUrl(fileSource: string): boolean {
-
-    var pattern = new RegExp('^(https?:\\/\\/)?' + // protocol
+    const pattern = new RegExp('^(https?:\\/\\/)?' + // protocol
       '((([a-z\\d]([a-z\\d-]*[a-z\\d])*)\\.?)+[a-z]{2,}|' + // domain name
       '((\\d{1,3}\\.){3}\\d{1,3}))' + // OR ip (v4) address
       '(\\:\\d+)?(\\/[-a-z\\d%_.~+]*)*' + // port and path
