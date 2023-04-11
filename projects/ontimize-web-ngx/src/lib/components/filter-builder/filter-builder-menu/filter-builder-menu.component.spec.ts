2a559322d33b57407161bb4cc3276906f19c7dc2
IMATIASL\patricia.martinez <patricia.martinez@imatia.com>
2023-04-11 10:27:43 +0200

    Resolved testing errrors

--- projects/ontimize-web-ngx/src/lib/components/filter-builder/filter-builder-menu/filter-builder-menu.component.spec.ts
+++ projects/ontimize-web-ngx/src/lib/components/filter-builder/filter-builder-menu/filter-builder-menu.component.spec.ts
@@ -2,7 +2,7 @@ import { Injector } from '@angular/core';
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 import { MatButtonModule } from '@angular/material/button';
 import { MatDialogModule } from '@angular/material/dialog';
-import { MatIconModule } from '@angular/material/icon';
+import { MatIconModule, MatIconRegistry } from '@angular/material/icon';
 import { MatMenuModule } from '@angular/material/menu';
 import { TranslateService } from '@ngx-translate/core';
 import { AppConfig, APP_CONFIG } from '../../../config/app-config';
@@ -12,6 +12,7 @@ import { appConfigFactory } from '../../../services';
 import { HttpClientModule } from '@angular/common/http';
 import { NoopAnimationsModule } from '@angular/platform-browser/animations';
 import { TestUtils } from '../../input/test/test-utils';
+import { FakeMatIconRegistry } from '../../input/test/fake-icon-registry';
 
 describe('FilterBuilderQueryButtonComponent', () => {
   let component: OFilterBuilderMenuComponent;
@@ -28,7 +29,8 @@ describe('FilterBuilderQueryButtonComponent', () => {
           deps: [Injector]
         },
         { provide: APP_CONFIG, useValue: TestUtils.mockConfiguration() },
-        { provide: AppConfig, useFactory: appConfigFactory, deps: [Injector] }
+        { provide: AppConfig, useFactory: appConfigFactory, deps: [Injector] },
+        { provide: MatIconRegistry, useClass: FakeMatIconRegistry }
       ]
     })
       .compileComponents();
