baf091e0469b36dee9f900901574279414f59a86
IMATIASL\patricia.martinez <patricia.martinez@imatia.com>
2023-04-28 18:16:31 +0200

    Removed unnecesary imports

--- projects/ontimize-web-ngx/src/lib/components/table/extensions/header/table-header-select-all/o-table-header-select-all.component.ts
+++ projects/ontimize-web-ngx/src/lib/components/table/extensions/header/table-header-select-all/o-table-header-select-all.component.ts
@@ -1,10 +1,8 @@
-import { ChangeDetectionStrategy, Component, forwardRef, Inject, ViewChild, ViewEncapsulation } from "@angular/core";
+import { ChangeDetectionStrategy, Component, forwardRef, Inject, ViewEncapsulation } from "@angular/core";
 import { OColumn } from "../../../column";
 import { OTableComponent } from "../../../o-table.component";
 import { DEFAULT_INPUTS_O_TABLE_HEADER, OTableHeaderComponent } from "../table-header/o-table-header.component";
-import { delay } from "rxjs/operators";
 import { Subscription } from "rxjs";
-
 
 @Component({
   selector: 'o-table-header-select-all',
@@ -28,7 +26,7 @@ export class OTableHeaderSelectAllComponent extends OTableHeaderComponent {
     @Inject(forwardRef(() => OTableComponent)) public table: OTableComponent
   ) {
     super(table);
-    this.selectionChangeSubscription =this.table.selection.changed.subscribe(x => {
+    this.selectionChangeSubscription = this.table.selection.changed.subscribe(x => {
       this.isAllSelected = table.isAllSelected();
       this.isIndeterminate = table.isIndeterminate();
     })
