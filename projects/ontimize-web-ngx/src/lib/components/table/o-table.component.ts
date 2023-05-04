1375a64e2d1aa7010067cea9de0bec16b16d08ec
Patricia Martinez <32088627+patricia-martinez-imatia@users.noreply.github.com>
2023-05-04 12:03:24 +0200

    Set title to select all header  (#1209)
    * New directive `o-table-column-select-all`
    
    * New component `o-table-header-select-all`
    
    * Removed unnecesary imports
    
    * Removed unexpected empty constructor
    
    * Improved css styles and header width
    
    * Improved css styles and header width
    
    * Improved methods isAllSelected and isIndeterminate
    
    * Resolving sonar bug
    
    * Resolving code smells
    
    * Resolving sonar bug
    
    * Revert "Resolving code smells"
    
    This reverts commit 041d555065d69a6d454d022b452d8153f321dffb.
    
    * Resolving sonar bug
    
    * Revert "Resolving sonar bug"
    
    This reverts commit 5b26ef13411b84ead3d81e9c624ee053a359ce1b.
    
    * Resolving bug fix sonar about unresolved promises
    
    * Revert "Resolving sonar bug"
    
    This reverts commit 086fe9489963c6c59b573636091b9704558c64f3.
    
    * Revert "Resolving bug fix sonar about unresolved promises"
    
    This reverts commit 0f67376699f0c88c456d3ad56b1e8eb1dc8d34dc.
    
    * Revert "Resolving sonar bug"
    
    This reverts commit 9b9de9b6a89205a061f2cd3d568b32ef304f3db4.
    
    * Added `o-table-column-select-all`
    
    * Renamed OTableColumnSelectAllComponent to OTableColumnSelectAllDirective
    
    * Added OTableHeaderSelectAllComponent
    
    * Removed extra comma

--- projects/ontimize-web-ngx/src/lib/components/table/o-table.component.ts
+++ projects/ontimize-web-ngx/src/lib/components/table/o-table.component.ts
@@ -104,6 +104,7 @@ import {
 } from './extensions/row/table-row-expandable/o-table-row-expandable.component';
 import { OMatSort } from './extensions/sort/o-mat-sort';
 import { O_TABLE_GLOBAL_CONFIG } from './utils/o-table.tokens';
+import { OTableColumnSelectAllDirective } from './extensions/header/table-column-select-all/o-table-column-select-all.directive';
 
 export const DEFAULT_INPUTS_O_TABLE = [
   ...DEFAULT_INPUTS_O_SERVICE_COMPONENT,
@@ -628,6 +629,9 @@ export class OTableComponent extends AbstractOServiceComponent<OTableComponentSt
   @ContentChild('o-table-quickfilter', { static: true })
   quickfilterContentChild: OTableQuickfilter;
 
+  @ContentChild(OTableColumnSelectAllDirective, { static: true })
+  tableColumnSelectAllContentChild: OTableColumnSelectAllDirective
+
   @ViewChild('exportOptsTemplate', { static: false })
   exportOptsTemplate: TemplateRef<any>;
 
@@ -681,7 +685,6 @@ export class OTableComponent extends AbstractOServiceComponent<OTableComponentSt
     super(injector, elRef, form);
 
     this._oTableOptions = new DefaultOTableOptions();
-    this._oTableOptions.selectColumn = this.createOColumn();
 
     try {
       this.tabGroupContainer = this.injector.get(MatTabGroup);
@@ -726,7 +729,31 @@ export class OTableComponent extends AbstractOServiceComponent<OTableComponentSt
       this.expandableItem = new SelectionModel<any>(this.tableRowExpandable.multiple, []);
       this.createExpandableColumn();
     }
+  }
 
+  ngAfterContentInit() {
+    if (this.tableColumnSelectAllContentChild) {
+      //
+      this.setCustomDefinitionInSelectColumn(this.tableColumnSelectAllContentChild)
+    }
+  }
+
+  setCustomDefinitionInSelectColumn(definition: OTableColumnSelectAllDirective) {
+    if (definition.title) {
+      this._oTableOptions.selectColumn.title = definition.title;
+    }
+    if (definition.width) {
+      this._oTableOptions.selectColumn.width = definition.width;
+    }
+    if (definition.minWidth) {
+      this._oTableOptions.selectColumn.minWidth = definition.minWidth;
+    }
+    if (definition.maxWidth) {
+      this._oTableOptions.selectColumn.maxWidth = definition.maxWidth;
+    }
+    if (definition.resizable) {
+      this._oTableOptions.selectColumn.resizable = definition.resizable;
+    }
   }
 
   ngAfterViewChecked() {
@@ -1979,14 +2006,14 @@ export class OTableComponent extends AbstractOServiceComponent<OTableComponentSt
   }
 
   public isAllSelected(): boolean {
-    const numSelected = this.selection.selected.length;
-    const numRows = this.getNumRowSelectedInCurrentData();
+    const numSelected = this.getNumRowSelectedInCurrentData();
+    const numRows = this.dataSource ? this.dataSource.renderedData.length : 0;
     return numSelected > 0 && numSelected === numRows;
   }
 
   public isIndeterminate(): boolean {
-    const numSelected = this.selection.selected.length;
-    const numRows = this.getNumRowSelectedInCurrentData();
+    const numSelected = this.getNumRowSelectedInCurrentData();
+    const numRows = this.dataSource ? this.dataSource.renderedData.length : 0;
     return numSelected > 0 && numRows > 0 && numSelected !== numRows;
   }
 
