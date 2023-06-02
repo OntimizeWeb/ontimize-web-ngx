903a6563f90dbfebe77e34192ea00bc0683f01e8
Patricia Martinez <32088627+patricia-martinez-imatia@users.noreply.github.com>
2023-06-02 14:59:44 +0200

    Migration to 12.x.x (#1221)
    * Updated versions @ngx-translate
    
    * Updated devDependencies
    
    * Updated libraries
    
    * removed webpack resolution
    
    * Installed @angular-devkit/core
    
    * Updated package-lock
    
    * Removed flex-layout
    
    * Updated angular.json
    
    * Migration to Angular 12
    
    * Updates Angular Material to v12.
    
    * Installed @angular/flex-layout@12.0.0-beta.34
    
    * Fixed sortbuttonLabel is removing
    
    * Updated version to 12
    
    * Updated peerDependencies
    
    * Reverted removing  `"emitDecoratorMetadata": true`
    
    * Fixed error `@use rules must be written before any other rules.`
    
    * Define $accent variable
    
    * Migration sass
    
    * New script build-theme
    
    * Updated node version to 14.20.0 in workflows
    
    * Updated node version to 14.20.0 in workflows
    
    * Updated node version to 14.20.0 in prepare release worflow
    
    * Updated peerDependencies
    
    * Fixed  DEPRECATION WARNING: Using / for division is deprecated and will be removed in Dart Sass 2.0.0.
    
    * Installed karma-coverage-istanbul-reporter
    
    * Installed karma-sonarqube-unit-reporter
    
    * Removed karma-coverage-istanbul-reporter because is deprecated
    
    * Fixed test in FakeMatIconRegistry
    
    * Configurated emitDecoratorMetadata in tsconfig
    
    * Improved karma.conf.js

--- projects/ontimize-web-ngx/karma.conf.js
+++ projects/ontimize-web-ngx/karma.conf.js
@@ -9,16 +9,19 @@ module.exports = function (config) {
       require('karma-jasmine'),
       require('karma-chrome-launcher'),
       require('karma-jasmine-html-reporter'),
-      require('karma-coverage-istanbul-reporter'),
+      require('karma-coverage'),
       require('@angular-devkit/build-angular/plugins/karma'),
       require('karma-sonarqube-unit-reporter')
     ],
     client: {
       clearContext: false // leave Jasmine Spec Runner output visible in browser
     },
-    coverageIstanbulReporter: {
+    coverageReporter: {
       dir: require('path').join(__dirname, '../../coverage/ontimize-web-ngx'),
-      reports: ['html', 'lcovonly', 'text-summary'],
+      subdir: '.',
+      reporters: [
+        { type: 'lcovonly' }
+      ],
       fixWebpackSourcePaths: true
     },
     sonarQubeUnitReporter: {
@@ -29,7 +32,7 @@ module.exports = function (config) {
       testFilePattern: '.spec.ts',
       useBrowserName: false
     },
-    reporters: ['progress', 'kjhtml', 'sonarqubeUnit'],
+    reporters: ['progress', 'kjhtml', 'sonarqubeUnit', 'coverage'],
     port: 9876,
     colors: true,
     logLevel: config.LOG_INFO,
