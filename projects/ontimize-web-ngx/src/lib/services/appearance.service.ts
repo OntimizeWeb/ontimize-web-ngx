d8315834aea872bf21a72fa4efd2269d8312acab
tomfacal <88186642+tomfacal@users.noreply.github.com>
2023-09-22 13:07:32 +0200

    added isDarkMode

--- projects/ontimize-web-ngx/src/lib/services/appearance.service.ts
+++ projects/ontimize-web-ngx/src/lib/services/appearance.service.ts
@@ -10,24 +10,28 @@ export class AppearanceService {
   private isDarkModeSubject = new BehaviorSubject<boolean>(false);
   isDarkMode$: Observable<boolean> = this.isDarkModeSubject.asObservable();
   protected _appConfig: AppConfig;
-  config: any;
 
   constructor(protected injector: Injector) {
     this._appConfig = this.injector.get<AppConfig>(AppConfig as Type<AppConfig>);
-    this.config = JSON.parse(localStorage.getItem(this._appConfig.getConfiguration().uuid));
+    const config = JSON.parse(localStorage.getItem(this._appConfig.getConfiguration().uuid));
 
-    if (this.config && this.config.theme && typeof this.config.theme.isDark === 'boolean') {
-      const isDark = this.config.theme.isDark;
+    if (config && config.theme && typeof config.theme.isDark === 'boolean') {
+      const isDark = config.theme.isDark;
       this.isDarkModeSubject.next(isDark);
     }
 
   }
 
   setDarkMode(isDarkMode: boolean) {
+    const config = JSON.parse(localStorage.getItem(this._appConfig.getConfiguration().uuid));
     this.isDarkModeSubject.next(isDarkMode);
-    if (this.config && this.config.theme) {
-      this.config.theme.isDark = isDarkMode;
+    if (config) {
+      config.theme.isDark = isDarkMode;
     }
-    localStorage.setItem(this._appConfig.getConfiguration().uuid, JSON.stringify(this.config));
+    localStorage.setItem(this._appConfig.getConfiguration().uuid, JSON.stringify(config));
+  }
+
+  isDarkMode(): boolean {
+    return this.isDarkModeSubject.value;
   }
 }
