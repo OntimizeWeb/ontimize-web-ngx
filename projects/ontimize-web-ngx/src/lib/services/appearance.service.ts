519d98f0d7f34b615fb6c451d9324fb0546d26e3
IMATIASL\patricia.martinez <patricia.martinez@imatia.com>
2023-10-04 21:40:59 +0200

    Set/Removed class name `o-dark`  in body tag

--- projects/ontimize-web-ngx/src/lib/services/appearance.service.ts
+++ projects/ontimize-web-ngx/src/lib/services/appearance.service.ts
@@ -1,20 +1,25 @@
-
-import { Injectable, Injector } from '@angular/core';
+import { DOCUMENT } from '@angular/common';
+import { inject, Injectable, Injector } from '@angular/core';
 import { BehaviorSubject, Observable } from 'rxjs';
+
 import { AppConfig } from '../config/app-config';
 import { LocalStorageService } from './local-storage.service';
+
 
 @Injectable({
   providedIn: 'root',
 })
 export class AppearanceService {
+  readonly darkThemeClass = 'o-dark';
+
   private isDarkModeSubject = new BehaviorSubject<boolean>(false);
   isDarkMode$: Observable<boolean> = this.isDarkModeSubject.asObservable();
   protected _appConfig: AppConfig;
+  protected _document: Document;
   protected localStorageService: LocalStorageService;
   constructor(protected injector: Injector) {
     this.localStorageService = this.injector.get(LocalStorageService);
-
+    this._document = inject(DOCUMENT);
     const config = this.localStorageService.getStoredData();
 
     if (config && config["theme"] && typeof config["theme"].isDark === 'boolean') {
@@ -30,11 +35,20 @@ export class AppearanceService {
       config["theme"] = config["theme"] || {};
       config["theme"].isDark = isDarkMode;
     }
+    this.updateThemeClass(isDarkMode);
     this.localStorageService.setLocalStorage(config);
     this.isDarkModeSubject.next(isDarkMode);
   }
 
   isDarkMode(): boolean {
     return this.isDarkModeSubject.value;
+  }
+
+  updateThemeClass(isDark?: boolean) {
+    if (isDark) {
+      this._document.body.classList.add(this.darkThemeClass);
+    } else {
+      this._document.body.classList.remove(this.darkThemeClass);
+    }
   }
 }
