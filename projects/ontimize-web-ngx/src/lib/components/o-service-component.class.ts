93dbfd911aa2abd2f6136b7acff5c970ba3bb898
IMATIASL\patricia.martinez <patricia.martinez@imatia.com>
2023-10-13 11:57:54 +0200

    Improved code

--- projects/ontimize-web-ngx/src/lib/components/o-service-component.class.ts
+++ projects/ontimize-web-ngx/src/lib/components/o-service-component.class.ts
@@ -213,7 +213,7 @@ export abstract class AbstractOServiceComponent<T extends AbstractComponentState
     }
   }
 
-  public quickFilterPlaceholder: string = undefined;
+  public quickFilterPlaceholder: string = '';
   /* end of inputs variables */
 
   /* outputs variables */
