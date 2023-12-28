91601900f0a4657895381ef9157c3bfdc081c064
Patricia Martinez <32088627+patricia-martinez-imatia@users.noreply.github.com>
2023-12-28 17:05:47 +0100

    New literals `REPORT.COLUMN.FALSEVALUE` and `REPORT.COLUMN.TRUEVALUE` (#1426)
    * New literals `REPORT.COLUMN.FALSEVALUE` and `REPORT.COLUMN.TRUEVALUE`
    
    * Fixed typo

--- projects/ontimize-web-ngx/src/lib/i18n/i18n.ts
+++ projects/ontimize-web-ngx/src/lib/i18n/i18n.ts
@@ -295,7 +295,9 @@ export const MAP = {
     'FILTERBUILDERMENU.BUTTONS.CLEAR_FILTER': 'Clear filter',
     'FILTERBUILDERMENU.DIALOG.CONFIRM_CLEAR_FILTER': 'This action will clear every filter on this section. Are you sure you want to continue?',
     'OIMAGE.TEXT.DROP&DRAG': 'Drag and drop image file here',
-    'OIMAGE.BUTTON.BROWSEFILE': 'Select image'
+    'OIMAGE.BUTTON.BROWSEFILE': 'Select image',
+    'REPORT.COLUMN.TRUEVALUE': 'Yes',
+    'REPORT.COLUMN.FALSEVALUE': 'No'
   },
 
   // Spanish
@@ -585,7 +587,9 @@ export const MAP = {
     'FILTERBUILDERMENU.BUTTONS.CLEAR_FILTER': 'Limpiar filtro',
     'FILTERBUILDERMENU.DIALOG.CONFIRM_CLEAR_FILTER': 'Esta acción limpiará todos los filtros de esta sección. ¿Desea continuar?',
     'OIMAGE.TEXT.DROP&DRAG': 'Arrastra y suelta la imagen aquí',
-    'OIMAGE.BUTTON.BROWSEFILE':'Seleccionar imagen'
+    'OIMAGE.BUTTON.BROWSEFILE': 'Seleccionar imagen',
+    'REPORT.COLUMN.TRUEVALUE': 'Si',
+    'REPORT.COLUMN.FALSEVALUE': 'No'
   },
   // Portuguese - BRAZIL
   pt: {
@@ -861,6 +865,8 @@ export const MAP = {
     'FILTERBUILDERMENU.BUTTONS.CLEAR_FILTER': 'Filtro limpo',
     'FILTERBUILDERMENU.DIALOG.CONFIRM_CLEAR_FILTER': 'Esta ação limpará todos os filtros nesta seção. Você tem certeza que quer continuar?',
     'OIMAGE.TEXT.DROP&DRAG': 'Arraste e solte a imagem aqui',
-    'OIMAGE.BUTTON.BROWSEFILE': 'Selecione uma imagem'
+    'OIMAGE.BUTTON.BROWSEFILE': 'Selecione uma imagem',
+    'REPORT.COLUMN.TRUEVALUE': 'Sim',
+    'REPORT.COLUMN.FALSEVALUE': 'Não'
   }
 }
