63802e6592a8a7a349a309f86243d6f03ec8bc1b
IMATIASL\patricia.martinez <patricia.martinez@imatia.com>
2024-01-23 09:56:27 +0100

    Added o-tree-node.component.spec.ts

--- projects/ontimize-web-ngx/src/lib/components/tree/tree-node/tree-node.component.spec.ts
+++ projects/ontimize-web-ngx/src/lib/components/tree/tree-node/tree-node.component.spec.ts
@@ -1,7 +1,25 @@
 /* tslint:disable:no-unused-variable */
+import { HttpClientModule } from '@angular/common/http';
+import { Injector } from '@angular/core';
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { MatDialogModule } from '@angular/material/dialog';
+import { RouterModule } from '@angular/router';
+import { TranslateService } from '@ngx-translate/core';
 
+import { APP_CONFIG, AppConfig } from '../../../config/app-config';
+import {
+  AbstractComponentStateService,
+  appConfigFactory,
+  AuthService,
+  OntimizeAuthServiceProvider,
+  OntimizeService,
+  OntimizeServiceResponseAdapter,
+  PermissionsService
+} from '../../../services';
+import { TestUtils } from '../../input/test/test-utils';
+import { OTreeDao } from '../o-tree-dao.service';
 import { OTreeNodeComponent } from './tree-node.component';
+
 
 describe('OTreeNodeComponent', () => {
   let component: OTreeNodeComponent;
@@ -9,7 +27,24 @@ describe('OTreeNodeComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ OTreeNodeComponent ]
+      declarations: [OTreeNodeComponent],
+      imports: [MatDialogModule, RouterModule.forRoot([]), HttpClientModule],
+      providers: [
+        {
+          provide: TranslateService,
+          useClass: TranslateService,
+          deps: [Injector]
+        },
+        OntimizeService,
+        OntimizeServiceResponseAdapter,
+        OTreeDao,
+        AuthService,
+        OntimizeAuthServiceProvider,
+        AbstractComponentStateService,
+        PermissionsService,
+        { provide: APP_CONFIG, useValue: TestUtils.mockConfiguration() },
+        { provide: AppConfig, useFactory: appConfigFactory, deps: [Injector] }
+      ]
     })
     .compileComponents();
   }));
