2695595125f4d26a941e16009da58ed3ec16897a
Patricia Martinez <32088627+patricia-martinez-imatia@users.noreply.github.com>
2024-02-20 17:17:42 +0100

    Skeleton in data components (#1491)
    * Installed ngx-skeleton-loader
    
    * new component `o-table-skeleton`
    
    * New `o-grid-skeleton` component
    
    * Removed duplicated code
    
    * new `o-list-skeleton` commponent
    
    * Removed  commented  code.
    
    * new OSkeletonComponent
    
    * Fixed spect.ts
    
    * Set background=transparent in skeleton container and improved code
    
    * Removed console.log
    
    * Removed commented code
    
    * Updated table skeleton styles
    
    * Improved getter count
    
    * Improved css

---
+++ projects/ontimize-web-ngx/src/lib/components/grid/skeketon/o-grid-skeleton/o-grid-skeleton.component.ts
@@ -0,0 +1,26 @@
+import { Component, ElementRef, ViewEncapsulation, Injector } from '@angular/core';
+import { OSkeletonComponent } from '../../../o-skeleton.component';
+
+@Component({
+  selector: 'o-grid-skeleton',
+  templateUrl: './o-grid-skeleton.component.html',
+  styleUrls: ['./o-grid-skeleton.component.scss'],
+  encapsulation: ViewEncapsulation.None,
+  host: {
+    '[class.o-grid-skeleton]': 'true'
+  }
+
+})
+export class OGridSkeletonComponent extends OSkeletonComponent {
+
+  constructor(protected elRef: ElementRef, protected injector: Injector) {
+    super(injector);
+  }
+
+  get count() {
+    const parentElement = this.elRef.nativeElement.parentElement;
+    /** 60+10+10+10*3 */
+    return Array(Math.floor(parentElement.offsetHeight / 175));
+
+  }
+}
---
+++ projects/ontimize-web-ngx/src/lib/components/table/extensions/skeleton/o-table-skeleton/o-table-skeleton.component.spec.ts
@@ -0,0 +1,38 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { OTableSkeletonComponent } from './o-table-skeleton.component';
+import { Injector } from '@angular/core';
+import { APP_CONFIG, AppConfig } from '../../../../../config/app-config';
+import { TestUtils } from '../../../../input/test/test-utils';
+import { appConfigFactory } from '../../../../../services/app-config.provider';
+import { AppearanceService } from '../../../../../services/appearance.service';
+import { AuthService } from '../../../../../services/auth.service';
+import { MatDialogModule } from '@angular/material/dialog';
+
+describe('OTableSkeletonComponent', () => {
+  let component: OTableSkeletonComponent;
+  let fixture: ComponentFixture<OTableSkeletonComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      imports: [MatDialogModule],
+      declarations: [OTableSkeletonComponent],
+      providers: [
+        { provide: APP_CONFIG, useValue: TestUtils.mockConfiguration() },
+        { provide: AppConfig, useFactory: appConfigFactory, deps: [Injector] },
+        AppearanceService,
+        AuthService
+        // ...INTERNAL_ONTIMIZE_MODULES
+      ]
+    })
+    .compileComponents();
+
+    fixture = TestBed.createComponent(OTableSkeletonComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
