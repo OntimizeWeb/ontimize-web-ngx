603980cf2b39006624e0dfb62a0223a97dff73c8
IMATIASL\patricia.martinez <patricia.martinez@imatia.com>
2024-03-15 15:53:06 +0100

    New RestService

--- projects/ontimize-web-ngx/src/lib/services/ontimize/index.ts
+++ projects/ontimize-web-ngx/src/lib/services/ontimize/index.ts
@@ -6,3 +6,4 @@ export * from './ontimize-file.service';
 export * from './ontimize-service-response.adapter';
 export * from './ontimize-service-response.class';
 export * from './ontimize.service';
+export * from './rest.service';
