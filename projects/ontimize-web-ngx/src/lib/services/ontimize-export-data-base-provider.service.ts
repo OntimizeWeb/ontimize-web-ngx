9ae15d7265afebbb9bdeb41faead3cc53dc192b6
tomfacal <88186642+tomfacal@users.noreply.github.com>
2024-04-25 09:12:08 +0200

    [15.0.4] Solved bug with exportation in non-pageable tables (#1575)
    * created method getFilterWithBasicExpression
    
    * protected code
    
    * refactor code

--- projects/ontimize-web-ngx/src/lib/services/ontimize-export-data-base-provider.service.ts
+++ projects/ontimize-web-ngx/src/lib/services/ontimize-export-data-base-provider.service.ts
@@ -1,5 +1,6 @@
 import { Injectable, Injector } from "@angular/core";
 import { OTableBase } from "../components/table/o-table-base.class";
+import { FilterExpressionUtils, Util } from "../util";
 
 @Injectable()
 export class OntimizeExportDataBaseProviderService {
@@ -37,8 +38,41 @@ export class OntimizeExportDataBaseProviderService {
     this.service = this.table.service;
 
     // Table filter
-    this.filter = this.table.getComponentFilter();
+    this.filter = this.getFilterWithBasicExpression();
   }
 
+  protected getFilterWithBasicExpression(): any {
+    let filter = this.table.getComponentFilter();
 
+    if (Object.keys(filter).length > 0) {
+      const parentItemExpr = FilterExpressionUtils.buildExpressionFromObject(filter);
+      filter = {};
+      filter[FilterExpressionUtils.FILTER_EXPRESSION_KEY] = parentItemExpr;
+    }
+    const beColFilter = this.table.getColumnFiltersExpression();
+    // Add column filters basic expression to current filter
+    if (beColFilter && !Util.isDefined(filter[FilterExpressionUtils.FILTER_EXPRESSION_KEY])) {
+      filter[FilterExpressionUtils.FILTER_EXPRESSION_KEY] = beColFilter;
+    } else if (beColFilter) {
+      filter[FilterExpressionUtils.FILTER_EXPRESSION_KEY] =
+        FilterExpressionUtils.buildComplexExpression(filter[FilterExpressionUtils.FILTER_EXPRESSION_KEY], beColFilter, FilterExpressionUtils.OP_AND);
+
+    }
+    const quickFilterExpr = Util.isDefined(this.table.oTableQuickFilterComponent) ? this.table.oTableQuickFilterComponent.filterExpression : undefined;
+
+    const filterBuilderExpr = Util.isDefined(this.table.filterBuilder) ? this.table.filterBuilder.getExpression() : undefined;
+    let complexExpr = quickFilterExpr || filterBuilderExpr;
+    if (quickFilterExpr && filterBuilderExpr) {
+      complexExpr = FilterExpressionUtils.buildComplexExpression(quickFilterExpr, filterBuilderExpr, FilterExpressionUtils.OP_AND);
+    }
+
+    if (complexExpr && !Util.isDefined(filter[FilterExpressionUtils.BASIC_EXPRESSION_KEY])) {
+      filter[FilterExpressionUtils.BASIC_EXPRESSION_KEY] = complexExpr;
+    } else if (complexExpr) {
+      filter[FilterExpressionUtils.BASIC_EXPRESSION_KEY] =
+        FilterExpressionUtils.buildComplexExpression(filter[FilterExpressionUtils.BASIC_EXPRESSION_KEY], complexExpr, FilterExpressionUtils.OP_AND);
+    }
+
+    return filter;
+  }
 }
