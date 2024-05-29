0bc207057bb1f1a71749349fd2a521665f1782ac
IMATIASL\patricia.martinez <patricia.martinez@imatia.com>
2024-05-29 14:36:50 +0200

    Improved code

--- projects/ontimize-web-ngx/src/lib/util/util.ts
+++ projects/ontimize-web-ngx/src/lib/util/util.ts
@@ -533,8 +533,17 @@ export class Util {
   }
 
   static sortFunction(propertyA: string | number, propertyB: string | number, activeSortDirection: string) {
-    const valueA = !Util.isDefined(propertyA) ? '' : propertyA === '' ? propertyA : isNaN(+propertyA) ? propertyA.toString().trim().toLowerCase() : +propertyA;
-    const valueB = !Util.isDefined(propertyB) ? '' : propertyB === '' ? propertyB : isNaN(+propertyB) ? propertyB.toString().trim().toLowerCase() : +propertyB;
+
+    let valueA: string | number = '';
+    let valueB: string | number = '';
+
+    if (Util.isDefined(propertyA)) {
+      valueA = isNaN(+propertyA) ? propertyA.toString().trim().toLowerCase() : +propertyA;
+    }
+
+    if (Util.isDefined(propertyB)) {
+      valueB = isNaN(+propertyB) ? propertyB.toString().trim().toLowerCase() : +propertyB;
+    }
     return (valueA <= valueB ? -1 : 1) * (activeSortDirection === 'asc' ? 1 : -1);
   }
 
