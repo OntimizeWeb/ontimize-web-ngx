6b0ff08d55c52e0a7c45829b9d732306b8969f63
IMATIASL\patricia.martinez <patricia.martinez@imatia.com>
2024-06-05 09:41:55 +0200

    Configure headerHeight with  O_GLOBAL_CONFIG injection token

--- projects/ontimize-web-ngx/src/lib/layouts/app-layout/o-app-layout.component.ts
+++ projects/ontimize-web-ngx/src/lib/layouts/app-layout/o-app-layout.component.ts
@@ -1,4 +1,4 @@
-import { AfterViewInit, Component, ContentChild, EventEmitter, ViewChild, ViewEncapsulation } from '@angular/core';
+import { AfterViewInit, Component, ContentChild, EventEmitter, Injector, ViewChild, ViewEncapsulation } from '@angular/core';
 import { ThemePalette } from '@angular/material';
 
 import { OAppHeaderComponent } from '../../components/app-header/o-app-header.component';
@@ -7,6 +7,7 @@ import { OUserInfoConfigurationDirective } from '../../components/user-info/user
 import { InputConverter } from '../../decorators/input-converter';
 import { Codes, OAppLayoutMode, OSidenavMode } from '../../util/codes';
 import { Util } from '../../util/util';
+import { O_GLOBAL_CONFIG } from '../../types/o-global-config.type';
 
 
 export const DEFAULT_INPUTS_O_APP_LAYOUT = [
@@ -44,6 +45,22 @@ export const DEFAULT_OUTPUTS_O_APP_LAYOUT: any[] = [
 })
 
 export class OAppLayoutComponent implements AfterViewInit {
+
+  constructor(protected injector: Injector) {
+    this.getGlobalInjectionTokenConfig();
+  }
+
+  private getGlobalInjectionTokenConfig() {
+    try {
+      const oGlobalConfig = this.injector.get(O_GLOBAL_CONFIG);
+
+      if (Util.isDefined(oGlobalConfig.rowHeight) && Codes.isValidRowHeight(oGlobalConfig.rowHeight)) {
+        this.headerHeight = oGlobalConfig.rowHeight;
+      };
+    } catch (error) {
+      // Do nothing because is optional
+    }
+  }
 
   @InputConverter()
   sidenavOpened: boolean = true;
