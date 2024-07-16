c01fb96bf892c14b3abfe5651842bef103235c49
IMATIASL\patricia.martinez <patricia.martinez@imatia.com>
2024-07-16 11:21:54 +0200

    Updated IPreferencesService

--- projects/ontimize-web-ngx/src/lib/interfaces/prefereces-service.interface.ts
+++ projects/ontimize-web-ngx/src/lib/interfaces/prefereces-service.interface.ts
@@ -3,6 +3,6 @@ import { Observable } from "rxjs";
 export interface IPreferencesService {
   saveAsPreferences(preferencesparams?: object): Observable<any>;
   savePreferences(id: number, preferencesparams?: object): Observable<any>;
-  getPreferences(entity?: string, service?: string): Observable<any>;
+  getPreferences(entity?: string, service?: string, type?: string): Observable<any>;
   deletePreferences(id?: number): Observable<any>;
 }
