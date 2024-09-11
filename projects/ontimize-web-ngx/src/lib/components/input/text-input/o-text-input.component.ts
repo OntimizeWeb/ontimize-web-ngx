7e11ed31e58c500b4e4d18e1ae023e2f27c8716e
Patricia Martinez <32088627+patricia-martinez-imatia@users.noreply.github.com>
2024-09-11 10:01:55 +0200

    Upper/lower case in text input (#1679)
    * New input string-case in o-text-input
    
    * New attribute `stringCase`  in OInputsOptions

--- projects/ontimize-web-ngx/src/lib/components/input/text-input/o-text-input.component.ts
+++ projects/ontimize-web-ngx/src/lib/components/input/text-input/o-text-input.component.ts
@@ -1,17 +1,32 @@
-import { Component, ContentChildren, ElementRef, forwardRef, Inject, Injector, OnInit, Optional, QueryList, ViewEncapsulation } from '@angular/core';
+import {
+  AfterViewInit,
+  Component,
+  ContentChildren,
+  ElementRef,
+  forwardRef,
+  Inject,
+  Injector,
+  OnDestroy,
+  OnInit,
+  Optional,
+  QueryList,
+  ViewEncapsulation
+} from '@angular/core';
 import { ValidatorFn, Validators } from '@angular/forms';
+import { Subscription } from 'rxjs';
 
 import { NumberConverter } from '../../../decorators/input-converter';
-import { OFormComponent } from '../../form/o-form.component';
-import {
-  OFormDataComponent
-} from '../../o-form-data-component.class';
 import { OMatPrefix } from '../../../directives/o-mat-prefix.directive';
 import { OMatSuffix } from '../../../directives/o-mat-suffix.directive';
+import { Util } from '../../../util/util';
+import { OFormComponent } from '../../form/o-form.component';
+import { OFormDataComponent } from '../../o-form-data-component.class';
 
 export const DEFAULT_INPUTS_O_TEXT_INPUT = [
   'minLength: min-length',
-  'maxLength: max-length'
+  'maxLength: max-length',
+  //uppercase | lowercase | default
+  'stringCase: string-case'
 ];
 
 
@@ -23,13 +38,16 @@ export const DEFAULT_INPUTS_O_TEXT_INPUT = [
   encapsulation: ViewEncapsulation.None
 })
 
-export class OTextInputComponent extends OFormDataComponent implements OnInit {
+export class OTextInputComponent extends OFormDataComponent implements OnInit, OnDestroy, AfterViewInit {
 
   @ContentChildren(OMatPrefix) _prefixChildren: QueryList<OMatPrefix>;
   @ContentChildren(OMatSuffix) _suffixChildren: QueryList<OMatSuffix>;
 
+  public stringCase: string;
+
   protected _minLength: number = -1;
   protected _maxLength: number = -1;
+  protected upperSubscription: Subscription;
 
   constructor(
     @Optional() @Inject(forwardRef(() => OFormComponent)) form: OFormComponent,
@@ -42,6 +60,24 @@ export class OTextInputComponent extends OFormDataComponent implements OnInit {
   ngOnInit() {
     super.ngOnInit();
   }
+
+  ngAfterViewInit(): void {
+    super.ngAfterViewInit();
+    this.initializeStringCase();
+  }
+
+  protected initializeStringCase() {
+    const stringCaseVariant = this.stringCase || this.oInputsOptions?.stringCase;
+    if (Util.isDefined(stringCaseVariant) && stringCaseVariant !== 'default') {
+      this.upperSubscription = this.getFormControl().valueChanges.subscribe((x: string) => {
+        if (Util.isDefined(x)) {
+          const value = stringCaseVariant === 'lowercase' ? x.toLowerCase() : x.toUpperCase();
+          this.setFormValue(value, { emitEvent: false })
+        }
+      })
+    }
+  }
+
 
   resolveValidators(): ValidatorFn[] {
     const validators: ValidatorFn[] = super.resolveValidators();
@@ -79,4 +115,12 @@ export class OTextInputComponent extends OFormDataComponent implements OnInit {
   get maxLength(): number {
     return this._maxLength;
   }
+
+  public ngOnDestroy(): void {
+    if (this.upperSubscription) {
+      this.upperSubscription.unsubscribe();
+    }
+  }
+
+
 }
