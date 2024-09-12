bf6b67c9844e9001abdd1dd9544be645c4abf7c5
IMATIASL\patricia.martinez <patricia.martinez@imatia.com>
2024-09-12 14:21:13 +0200

    New injection token `O_LOCALSTORAGE_SERVICE` to extends LocalStorageService

--- projects/ontimize-web-ngx/src/lib/config/o-providers.ts
+++ projects/ontimize-web-ngx/src/lib/config/o-providers.ts
@@ -6,7 +6,7 @@ import { combineLatest } from 'rxjs';
 
 import { AppConfig, O_INPUTS_OPTIONS } from '../config/app-config';
 import { appConfigFactory } from '../services/app-config.provider';
-import { ComponentStateServiceProvider, ExportDataServiceProvider, OntimizeAuthServiceProvider, OntimizeExportServiceProvider, OntimizeServiceProvider, O_MAT_ERROR_OPTIONS } from '../services/factories';
+import { ComponentStateServiceProvider, ExportDataServiceProvider, OntimizeAuthServiceProvider, OntimizeExportServiceProvider, OntimizeServiceProvider, O_MAT_ERROR_OPTIONS, OntimizeLocalStorageServiceProvider } from '../services/factories';
 import { LocalStorageService } from '../services/local-storage.service';
 import { NavigationService } from '../services/navigation.service';
 import { OntimizeMatIconRegistry } from '../services/ontimize-icon-registry.service';
@@ -82,6 +82,7 @@ export const ONTIMIZE_PROVIDERS: Provider[] = [
   ComponentStateServiceProvider,
   ExportDataServiceProvider,
   OntimizeExportServiceProvider,
+  OntimizeLocalStorageServiceProvider,
   // disabled global ripple
   { provide: MAT_RIPPLE_GLOBAL_OPTIONS, useValue: { disabled: true } },
   { provide: O_MAT_ERROR_OPTIONS, useValue: {} },
