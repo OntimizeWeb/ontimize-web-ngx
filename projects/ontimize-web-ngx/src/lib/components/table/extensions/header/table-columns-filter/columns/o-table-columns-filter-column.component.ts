43ddcf0de849d878f98971ea48e0e27d44626bdd
IMATIASL\patricia.martinez <patricia.martinez@imatia.com>
2024-09-25 11:40:41 +0200

    new input query-method in o-table-columns-filter-column

--- projects/ontimize-web-ngx/src/lib/components/table/extensions/header/table-columns-filter/columns/o-table-columns-filter-column.component.ts
+++ projects/ontimize-web-ngx/src/lib/components/table/extensions/header/table-columns-filter/columns/o-table-columns-filter-column.component.ts
@@ -5,6 +5,7 @@ export type OFilterColumn = {
   attr: string;
   sort: 'asc' | 'desc' | '';
   startView: 'month' | 'year' | 'multi-year' | '';
+  queryMethod?: string;
 };
 
 export const DEFAULT_INPUTS_O_TABLE_COLUMN_FILTER_COLUMN = [
@@ -13,7 +14,8 @@ export const DEFAULT_INPUTS_O_TABLE_COLUMN_FILTER_COLUMN = [
   // sort [asc|desc]: initial sorting, with the format column:[ASC|DESC].
   'sort',
   // startView [month|year|multi-year]: Datepicker initial view in case of date column.
-  'startView:start-view'
+  'startView:start-view',
+  'queryMethod:query-method'
 ];
 
 @Component({
@@ -28,5 +30,6 @@ export class OTableColumnsFilterColumnComponent {
   public attr: string = '';
   public sort: 'asc' | 'desc' | '' = '';
   public startView: 'month' | 'year' | 'multi-year' | '' = 'month';
+  public queryMethod: string;
 
 }
