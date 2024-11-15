8d8e7880bb0673e362434ba75b3db31bae1b3b98
IMATIASL\patricia.martinez <patricia.martinez@imatia.com>
2024-11-15 13:40:01 +0100

    Resolved code smells

--- projects/ontimize-web-ngx/src/lib/components/table/extensions/header/table-button/o-table-button.component.ts
+++ projects/ontimize-web-ngx/src/lib/components/table/extensions/header/table-button/o-table-button.component.ts
@@ -61,10 +61,9 @@ export class OTableButtonComponent implements OTableButton, OnInit {
     event.stopPropagation();
     if (!this.isReadOnly()) {
       this.onClick.emit();
-    } else {
-      if (this._table.showNotificationOfReadOnly) {
+    } else if (this._table.showNotificationOfReadOnly) {
         this.table.getSnackService().open('MESSAGES.OPERATION_NOT_ALLOWED_READONLY');
-      }
+
     }
   }
 
