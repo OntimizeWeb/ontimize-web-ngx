41d2b77417b98637af97d348e3f0621039408dd9
IMATIASL\patricia.martinez <patricia.martinez@imatia.com>
2025-01-30 12:48:35 +0100

    Merge commits from 15.x.x

--- projects/ontimize-web-ngx/src/lib/components/table/extensions/skeleton/o-table-skeleton.component.ts
+++ projects/ontimize-web-ngx/src/lib/components/table/extensions/skeleton/o-table-skeleton.component.ts
@@ -1,4 +1,4 @@
-import { Component, ElementRef, Injector, OnInit, ViewEncapsulation } from '@angular/core';
+import { AfterViewInit, ChangeDetectorRef, Component, ElementRef, Injector, OnInit, ViewEncapsulation } from '@angular/core';
 import { OSkeletonComponent } from '../../../o-skeleton.component';
 import { Observable, of } from 'rxjs';
 import { Util } from '../../../../util/util';
@@ -14,14 +14,20 @@ import { Util } from '../../../../util/util';
   }
 
 })
-export class OTableSkeletonComponent extends OSkeletonComponent implements OnInit {
+export class OTableSkeletonComponent extends OSkeletonComponent implements OnInit, AfterViewInit {
   rows$: Observable<number[]>;
-
+  private readonly cd: ChangeDetectorRef;
   constructor(protected elRef: ElementRef, protected injector: Injector) {
     super(injector);
+    this.cd = injector.get(ChangeDetectorRef);
   }
 
   ngOnInit(): void {
+    this.rows$ = of([1]);
+  }
+
+  ngAfterViewInit(): void {
+    this.cd.detectChanges();// sure detect changes
     this.rows$ = of(this.getRows());
   }
 
@@ -30,12 +36,37 @@ export class OTableSkeletonComponent extends OSkeletonComponent implements OnIni
     const parentElement = this.elRef.nativeElement.parentElement;
 
     /* available parentHeight = parentElement height  - (header table header height + margin bottom)*/
-    const parentHeight = parentElement?.offsetHeight - 60;
+    const header = parentElement.querySelector('div.o-table-skeleton-header');
+    const item = parentElement.querySelector('div.o-table-skeleton-item');
+
+    let totalHeightHeader: number;
+    let totalHeightItem: number;
+    if (header) {
+      // Obtén las dimensiones y estilos computados del elemento
+      const headerComputedStyle = getComputedStyle(header);
+      const height = header.offsetHeight; // Altura incluyendo padding
+      const marginTop = parseFloat(headerComputedStyle.marginTop); // Margen superior
+      const marginBottom = parseFloat(headerComputedStyle.marginBottom); // Margen inferior
+
+      // Calcula la altura total
+      totalHeightHeader = height + marginBottom + marginTop;
+    }
+    if (item) {
+      // Obtén las dimensiones y estilos computados del elemento
+      const itemComputedStyle = getComputedStyle(item);
+      const height = item.offsetHeight; // Altura incluyendo padding
+      const marginBottom = parseFloat(itemComputedStyle.marginBottom); // Margen inferior
+
+      // Calcula la altura total
+      totalHeightItem = height + marginBottom;
+    }
+
+    const parentHeight = parentElement?.offsetHeight - totalHeightHeader;
     if (!Util.isDefined(parentHeight) || parentHeight < 0) {
       return [];
     }
 
-    return Array.from(new Array(Math.floor(parentHeight / 30)), (x, i) => i + 1);
+    return Array.from(new Array(Math.floor(parentHeight / totalHeightItem)), (x, i) => i + 1);
 
   }
 
