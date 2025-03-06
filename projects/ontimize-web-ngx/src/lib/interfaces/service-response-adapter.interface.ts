2c72645aeba36d6573923aedc36593cf31bf03be
IMATIASL\patricia.martinez <patricia.martinez@imatia.com>
2025-03-06 13:22:51 +0100

    Changed to optional the  adaptError method

--- projects/ontimize-web-ngx/src/lib/interfaces/service-response-adapter.interface.ts
+++ projects/ontimize-web-ngx/src/lib/interfaces/service-response-adapter.interface.ts
@@ -1,7 +1,6 @@
 import { HttpErrorResponse, HttpResponse } from '@angular/common/http';
 
 export interface IServiceResponseAdapter<T> {
-  context: any;
   adapt(res: HttpResponse<any>): T;
-  adaptError(error: HttpErrorResponse): any
+  adaptError?(error: HttpErrorResponse): any
 }
