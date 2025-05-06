90c51517eca5666b633fbccfbded4b351e926f01
IMATIASL\patricia.martinez <patricia.martinez@imatia.com>
2025-05-06 09:32:36 +0200

    Fixed scroll resets when showing skeleton

--- projects/ontimize-web-ngx/src/lib/components/grid/skeketon/o-grid-skeleton.component.ts
+++ projects/ontimize-web-ngx/src/lib/components/grid/skeketon/o-grid-skeleton.component.ts
@@ -1,4 +1,6 @@
-import { Component, ElementRef, ViewEncapsulation, Injector } from '@angular/core';
+import { AfterViewInit, Component, ViewEncapsulation } from '@angular/core';
+
+import { Util } from '../../../util/util';
 import { OSkeletonComponent } from '../../o-skeleton.component';
 
 @Component({
@@ -11,16 +13,35 @@ import { OSkeletonComponent } from '../../o-skeleton.component';
   }
 
 })
-export class OGridSkeletonComponent extends OSkeletonComponent {
+export class OGridSkeletonComponent extends OSkeletonComponent implements AfterViewInit {
 
-  constructor(protected elRef: ElementRef, protected injector: Injector) {
-    super(injector);
-  }
 
-  get count() {
+  getRows() {
+
     const parentElement = this.elRef.nativeElement.parentElement;
-    /** 60+10+10+10*3 */
-    return Array(Math.floor(parentElement.offsetHeight / 175));
+
+    /* available parentHeight = parentElement height  - (header table header height + margin bottom)*/
+    const item = parentElement.querySelector('div.o-grid-skeleton-item');
+
+    let totalHeightItem: number = 0;
+
+    if (item) {
+      // Obtén las dimensiones y estilos computados del elemento
+      const itemComputedStyle = getComputedStyle(item);
+      const height = item.offsetHeight; // Altura incluyendo padding
+      const marginBottom = parseFloat(itemComputedStyle.marginBottom); // Margen inferior
+      const marginTop = parseFloat(itemComputedStyle.marginTop);
+
+      // Calcula la altura total
+      totalHeightItem = height + marginBottom + marginTop;
+    }
+
+    const availableHeight = parentElement?.offsetHeight - totalHeightItem;
+    if (!Util.isDefined(availableHeight) || availableHeight < 0) {
+      return [];
+    }
+
+    return Array.from(new Array(Math.ceil(parentElement?.offsetHeight / totalHeightItem)), (x, i) => i + 1);
 
   }
 }
