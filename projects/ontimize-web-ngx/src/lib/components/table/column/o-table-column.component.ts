66078a17632e90f6bbdcca1eb0b28e3acc7744de
Patricia Martinez <32088627+patricia-martinez-imatia@users.noreply.github.com>
2025-05-28 09:34:27 +0200

    [15.5.5] Fixed min and max validator in `o-table-cell-editor-real` (#1910)
    * Fixes #1904
    
    * Removed console.log

--- projects/ontimize-web-ngx/src/lib/components/table/column/o-table-column.component.ts
+++ projects/ontimize-web-ngx/src/lib/components/table/column/o-table-column.component.ts
@@ -433,8 +433,12 @@ export class OTableColumnComponent implements OTableColumn, OnDestroy, OnInit, A
         case 'percentage':
         case 'currency':
         case 'real':
-          editor.min = propsOrigin.min;
-          editor.max = propsOrigin.max;
+          if (Util.isDefined(propsOrigin.min)) {
+            editor.min = propsOrigin.min;
+          }
+          if (Util.isDefined(propsOrigin.max)) {
+            editor.max = propsOrigin.max;
+          }
           editor.step = Util.isDefined(propsOrigin.step) ? propsOrigin.step : editor.step;
           break;
         case 'image':
