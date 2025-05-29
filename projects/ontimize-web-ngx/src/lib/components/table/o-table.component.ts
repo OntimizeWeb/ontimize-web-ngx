9d121f79c55259ed3a2b661e38a4936fd5e13a00
IMATIASL\patricia.martinez <patricia.martinez@imatia.com>
2025-05-29 17:23:09 +0200

    Fixes #1906

--- projects/ontimize-web-ngx/src/lib/components/table/o-table.component.ts
+++ projects/ontimize-web-ngx/src/lib/components/table/o-table.component.ts
@@ -5,6 +5,7 @@ import { DomPortalOutlet, TemplatePortal } from '@angular/cdk/portal';
 import { CdkVirtualScrollViewport, VIRTUAL_SCROLL_STRATEGY } from '@angular/cdk/scrolling';
 import {
   AfterContentInit,
+  AfterViewChecked,
   AfterViewInit,
   ApplicationRef,
   ChangeDetectionStrategy,
@@ -102,6 +103,7 @@ import type { OTableOptions } from '../../interfaces/o-table-options.interface';
 import type { OTablePaginator } from '../../interfaces/o-table-paginator.interface';
 import type { OTableQuickfilter } from '../../interfaces/o-table-quickfilter.interface';
 import type { ServiceResponse } from '../../interfaces/service-response.interface';
+import { MatRow } from '@angular/material/table';
 export const DEFAULT_INPUTS_O_TABLE = [
   // visible-columns [string]: visible columns, separated by ';'. Default: no value.
   'visibleColumns: visible-columns',
@@ -279,7 +281,7 @@ type DisableSelectionFunction = (item: any) => boolean;
     '(document:click)': 'handleDOMClick($event)'
   }
 })
-export class OTableComponent extends AbstractOServiceComponent<OTableComponentStateService> implements OnInit, OnDestroy, AfterViewInit, AfterContentInit {
+export class OTableComponent extends AbstractOServiceComponent<OTableComponentStateService> implements OnInit, OnDestroy, AfterViewChecked, AfterViewInit, AfterContentInit {
   public static DEFAULT_BASE_SIZE_SPINNER = 100;
   public static FIRST_LAST_CELL_PADDING = 24;
   public static EXPANDED_ROW_CONTAINER_CLASS = 'expanded-row-container-';
@@ -297,6 +299,8 @@ export class OTableComponent extends AbstractOServiceComponent<OTableComponentSt
   dblclickSubject = new Subject<{ row: any, column: any, cellRef: any, rowIndex: number, event: MouseEvent }>();
   protected clickSubjectSubscription: Subscription;
   protected dbClickSubjectSubscription: Subscription;
+  subscriptionOnDataLoaded: Subscription;
+  subscriptionOnPaginationDataLoaded: any;
 
   @ViewChild(OMatSort)
   set oMatSort(_sort: OMatSort) {
@@ -763,16 +767,27 @@ export class OTableComponent extends AbstractOServiceComponent<OTableComponentSt
     }
 
     if (this.tableRowExpandable) {
-      this.expandableItem = new SelectionModel<any>(this.tableRowExpandable.multiple, []);
+      this.expandableItem = new SelectionModel<any>(this.tableRowExpandable.multiple, [], true, this.compareRow());
       this.createExpandableColumn();
+      this.expandableItem.changed.subscribe((change) => {
+        this.saveRowExpandState(change.added, change.removed);
+      });
     }
+    this.rows.changes.subscribe(() => {
+      this.handleTableDataChange();
+    });
+
+
   }
 
   ngAfterContentInit() {
     if (this.tableColumnSelectAllContentChild) {
-      //
       this.setCustomDefinitionInSelectColumn(this.tableColumnSelectAllContentChild)
     }
+  }
+
+  handleTableDataChange() {
+    this.restoreExpandableRowState();
   }
 
   setCustomDefinitionInSelectColumn(definition: OTableColumnSelectAllDirective) {
@@ -1081,6 +1096,14 @@ export class OTableComponent extends AbstractOServiceComponent<OTableComponentSt
 
     if (this.dbClickSubjectSubscription) {
       this.dbClickSubjectSubscription.unsubscribe();
+    }
+
+    if (this.subscriptionOnDataLoaded) {
+      this.subscriptionOnDataLoaded.unsubscribe();
+    }
+
+    if (this.subscriptionOnPaginationDataLoaded) {
+      this.subscriptionOnPaginationDataLoaded.unsubscribe();
     }
 
     Object.keys(this.asyncLoadSubscriptions).forEach(idx => {
@@ -1546,6 +1569,40 @@ export class OTableComponent extends AbstractOServiceComponent<OTableComponentSt
       this.portalHost[rowIndex].attachTemplatePortal(templatePortal);
       this.tableRowExpandable.onExpanded.emit(eventTableRowExpandableChange);
     }
+
+  }
+
+  saveRowExpandState(added: any[], removed: any[]) {
+    let state = this.state.expandableRows ?? [];
+
+    const extractKeys = (item: any) =>
+      Object.fromEntries(Object.entries(item).filter(([key]) => this.keysArray.includes(key)));
+
+    const isSameRow = this.compareRow();
+
+    // Add expandable items
+    if (added?.length) {
+      added.forEach(item => {
+        const rowIndex = this.getValue().findIndex(row => isSameRow(row, item));
+        const alreadyExists = state.some(stateItem => isSameRow(stateItem.keys, item));
+
+        if (rowIndex !== -1 && !alreadyExists) {
+          state.push({ keys: extractKeys(item) });
+        }
+      });
+    }
+
+    // Delete expandable items
+    if (removed?.length) {
+      removed.forEach(item => {
+        const indexToRemove = state.findIndex(stateItem => isSameRow(stateItem.keys, item));
+        if (indexToRemove !== -1) {
+          state.splice(indexToRemove, 1);
+        }
+      });
+    }
+
+    this.state.expandableRows = state;
   }
 
   destroyAllPortalHosts(): void {
@@ -1567,7 +1624,7 @@ export class OTableComponent extends AbstractOServiceComponent<OTableComponentSt
   }
 
 
-
+  @ViewChildren(MatRow) rows!: QueryList<MatRow>;
 
   private emitTableRowExpandableChangeEvent(data, rowIndex) {
     const event = new OTableRowExpandedChange();
@@ -1764,6 +1821,23 @@ export class OTableComponent extends AbstractOServiceComponent<OTableComponentSt
 
     if (this.state.selection && this.dataSource.renderedData.length > 0 && this.getSelectedItems().length === 0) {
       this.checkSelectedItemData();
+    }
+
+  }
+
+  restoreExpandableRowState(): void {
+
+    if (this.tableRowExpandable && this.state.expandableRows) {
+      this.state.expandableRows.forEach(expandableRow => {
+        const data = this.getRenderedValue();;
+        if (data.length > 0) {
+
+          const rowIndex = data.findIndex((row) => this.keysArray.every(key => row[key] === expandableRow.keys[key]));
+          if (data.length > rowIndex) {
+            this.toggleRowExpandable(data[rowIndex]);
+          }
+        }
+      });
     }
   }
 
