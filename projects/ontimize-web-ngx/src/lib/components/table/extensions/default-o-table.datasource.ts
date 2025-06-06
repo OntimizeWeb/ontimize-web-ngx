a7711da91acf9f01c124ff65b3e7bce55a9eb755
IMATIASL\patricia.martinez <patricia.martinez@imatia.com>
2025-06-06 09:13:05 +0200

    Removed console.log

--- projects/ontimize-web-ngx/src/lib/components/table/extensions/default-o-table.datasource.ts
+++ projects/ontimize-web-ngx/src/lib/components/table/extensions/default-o-table.datasource.ts
@@ -484,7 +484,6 @@ export class DefaultOTableDataSource extends DataSource<any> implements OTableDa
   }
 
   addColumnFilter(filter: OColumnValueFilter) {
-    console.log('addColumnFilter' , filter);
     const existingFilterIndex = this.columnValueFilters.findIndex(item => item.attr === filter.attr);
     if (existingFilterIndex > -1) {
       this.columnValueFilters.splice(existingFilterIndex, 1, filter);
