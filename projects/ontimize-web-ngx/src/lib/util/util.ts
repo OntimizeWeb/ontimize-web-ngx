b261f0909c771ff706b654055a08510cc677d6a8
Patricia Martinez <32088627+patricia-martinez-imatia@users.noreply.github.com>
2025-06-13 10:00:59 +0200

    o-table-column: new input value-column (#1926)
    * Fixes #1925
    
    * Removed unnecsary variable
    
    * Revert incorrect format

--- projects/ontimize-web-ngx/src/lib/util/util.ts
+++ projects/ontimize-web-ngx/src/lib/util/util.ts
@@ -573,4 +573,14 @@ export class Util {
     }
     return child;
   }
+
+  static getValueFromPath(obj: any, path: string): any {
+    if (!obj || !path) return undefined;
+    return path.split('.').reduce((acc, key) => {
+      if (acc && typeof acc === 'object') {
+        return acc[key];
+      }
+      return undefined;
+    }, obj);
+  }
 }
