5fa24f3e9fced149d783de33e39cee275cb0fde7
Patricia Martinez Tilve <patricia.martinez@imatia.com>
2025-07-23 13:13:53 +0200

    Removed unused import

--- projects/ontimize-web-ngx/src/lib/util/util.ts
+++ projects/ontimize-web-ngx/src/lib/util/util.ts
@@ -6,16 +6,10 @@ import { IDataService } from '../interfaces/data-service.interface';
 import { IFormDataComponent } from '../interfaces/form-data-component.interface';
 import { IPermissionsService } from '../interfaces/permissions-service.interface';
 import { OConfigureMessageServiceArgs } from '../types/configure-message-service-args.type';
-import { OConfigureServiceArgs } from '../types/configure-service-args.type';
 import { ODateValueType } from '../types/o-date-value.type';
 import { Base64 } from './base64';
 import { Codes } from './codes';
 import { ActivatedRouteSnapshot } from '@angular/router';
-import { AppConfig } from '../config/app-config';
-import { ServiceType } from '../types/service-type.type';
-import { JSONAPIService } from '../services/jsonapi/jsonapi.service';
-import { OntimizeEEService } from '../services/ontimize/ontimize-ee.service';
-import { OntimizeService } from '../services/ontimize/ontimize.service';
 
 export class Util {
 
