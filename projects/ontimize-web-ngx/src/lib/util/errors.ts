217abea71262c07cbafba63a6180ac091231e95e
Patricia Martinez Tilve <patricia.martinez@imatia.com>
2025-07-23 11:41:47 +0200

    Improved code

--- projects/ontimize-web-ngx/src/lib/util/errors.ts
+++ projects/ontimize-web-ngx/src/lib/util/errors.ts
@@ -23,12 +23,8 @@ export interface ComponentWithValidatorsAndErrorsData {
 
 export class ErrorsUtils {
   static getErrorOptions(injector: Injector): OMatErrorOptions {
-    let result: OMatErrorOptions
-    try {
-      result = injector.get(O_MAT_ERROR_OPTIONS) || {};
-    } catch (e) {
-      result = {};
-    }
+    const result = injector.get(O_MAT_ERROR_OPTIONS, {});
+
     if (!Util.isDefined(result.type)) {
       result.type = Codes.O_MAT_ERROR_STANDARD as OMatErrorType;
     }
