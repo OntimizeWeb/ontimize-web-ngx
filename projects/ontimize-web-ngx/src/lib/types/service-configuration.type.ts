81c938b19163d27916eef8a4f733739e07c6f61a
Patricia Martinez <32088627+patricia-martinez-imatia@users.noreply.github.com>
2025-07-29 09:41:59 +0200

    Redefined serviceConfiguration type in config.type.ts (#1950)
    * Redefined serviceConfiguration type in config.type.ts
    
    * Updated comments in service-configuration.type.ts

--- projects/ontimize-web-ngx/src/lib/types/service-configuration.type.ts
+++ projects/ontimize-web-ngx/src/lib/types/service-configuration.type.ts
@@ -1,15 +1,24 @@
+import { SessionInfo } from "./session-info.type";
 
-export type ServiceConfigType = {
+export type ServiceConfig = {
   [key: string]: {
     path: string;
     serviceType?: string;
+    /** URL used globally for sending HTTP requests */
+    urlBase?: string;
+    /** Session information */
+    session?: SessionInfo;
+    /* (Ontimize Boot 2.x or earlier) API endpoint used to generate export files (e.g., CSV, Excel, PDF) from an o-table.*/
+    exportPath?: string;
+    /* (Ontimize Boot 2.x or earlier) API endpoint used to download a previously exported file via its file ID.*/
+    downloadPath?:string
   };
 };
-export type OntimizeServiceConfigType = ServiceConfigType & {
+export type OntimizeServiceConfig = ServiceConfig & {
   fileManagerPath?: string;
 };
 
-export type JSONAPIServiceConfigType = ServiceConfigType & {
+export type JSONAPIServiceConfig = ServiceConfig & {
   delimiter?: string;
 };
 
