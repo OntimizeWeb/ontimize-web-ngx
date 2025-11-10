35121597c1cbdf85ace69c8c1471429e8a4109b1
Patricia Martinez Tilve <patricia.martinez@imatia.com>
2025-11-10 15:45:59 +0100

    Formatted code

--- projects/ontimize-web-ngx/src/lib/components/table/column/cell-editor/time/o-table-cell-editor-time.component.ts
+++ projects/ontimize-web-ngx/src/lib/components/table/column/cell-editor/time/o-table-cell-editor-time.component.ts
@@ -324,25 +324,24 @@ export class OTableCellEditorTimeComponent extends OBaseTableCellEditor implemen
       });
 
 
+      this.picker.closed
+        .pipe(takeUntil(this.destroy$))
+        .subscribe(() => {
+          this.cd.detectChanges();
 
-    this.picker.closed
-      .pipe(takeUntil(this.destroy$))
-      .subscribe(() => {
-        this.cd.detectChanges();
+          // Another solution was to add a 200ms delay, but the following is a better solution as it doesn't add any delays.
+          setTimeout(() => {
+            queueMicrotask(() => {
+              const input: HTMLInputElement | null =
+                this.hourInput?.nativeElement?.querySelector?.('input') ||
+                this.hourInput?.nativeElement;
 
-        // Another solution was to add a 200ms delay, but the following is a better solution as it doesn't add any delays.
-        setTimeout(() => {
-          queueMicrotask(() => {
-            const input: HTMLInputElement | null =
-              this.hourInput?.nativeElement?.querySelector?.('input') ||
-              this.hourInput?.nativeElement;
-
-            if (input && !input.disabled) {
-              input.focus({ preventScroll: true });
-            }
-          });
-        }, 0);
-      });
+              if (input && !input.disabled) {
+                input.focus({ preventScroll: true });
+              }
+            });
+          }, 0);
+        });
     }
   }
 
