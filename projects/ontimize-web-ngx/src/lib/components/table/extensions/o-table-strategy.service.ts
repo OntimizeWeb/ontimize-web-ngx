3cadd31b8d9ce9b813989dc0796ee6c6c0ca8fb9
Patricia Martinez Tilve <patricia.martinez@imatia.com>
2026-01-22 08:53:27 +0100

    Fixes #2005

--- projects/ontimize-web-ngx/src/lib/components/table/extensions/o-table-strategy.service.ts
+++ projects/ontimize-web-ngx/src/lib/components/table/extensions/o-table-strategy.service.ts
@@ -15,6 +15,7 @@ export class OTableVirtualScrollStrategy implements VirtualScrollStrategy {
   public readonly stickyChange = new Subject<number>();
   private readonly bufferMultiplier: number = 1;
   private lastRenderedRange = { start: 0, end: 0 };
+  private savedScrollPosition = 0;
 
   get dataLength(): number {
     return this._dataLength;
@@ -22,7 +23,10 @@ export class OTableVirtualScrollStrategy implements VirtualScrollStrategy {
 
   set dataLength(value: number) {
     this._dataLength = value;
-    this.onDataLengthChanged();
+    if (this.savedScrollPosition === 0) {
+      this.onDataLengthChanged();
+    }
+    this.savedScrollPosition = 0;
   }
 
   private _dataLength = 0;
@@ -56,6 +60,10 @@ export class OTableVirtualScrollStrategy implements VirtualScrollStrategy {
 
   public onContentScrolled(): void {
     this.updateContent();
+  }
+
+  setSavedScrollPosition(value: number) {
+    this.savedScrollPosition = value;
   }
 
   public setConfig(rowHeight: number, headerHeight: number, footerHeight: number) {
