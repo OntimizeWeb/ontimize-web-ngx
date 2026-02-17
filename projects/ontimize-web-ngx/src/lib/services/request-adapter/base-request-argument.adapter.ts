4edfae02c3f44f25658cc13998de6503457c4b7d
Patricia Martinez Tilve <patricia.martinez@imatia.com>
2026-02-17 19:23:51 +0100

    Resolves #2075

--- projects/ontimize-web-ngx/src/lib/services/request-adapter/base-request-argument.adapter.ts
+++ projects/ontimize-web-ngx/src/lib/services/request-adapter/base-request-argument.adapter.ts
@@ -1,7 +1,13 @@
 import { Observable } from "rxjs";
 import { ServiceResponse } from "../../interfaces/service-response.interface";
+import { PaginationContextService } from "../pagination-context.service";
 
 export class BaseRequestArgument {
+  protected paginationContextService: PaginationContextService;
+
+  setPaginationContextService(service: PaginationContextService): void {
+    this.paginationContextService = service;
+  }
 
   request(method: string, service: any, queryArguments: any) {
     return (service[method](...queryArguments) as Observable<ServiceResponse>);
